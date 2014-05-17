fca2cd8e58f76291dc92423942f47eb38b5af869
Sam Georges <sam@daftspunk.com>
2014-05-17 13:04:59 +1000

    Remove adaption on server.php, original Laravel copy works fine

--- server.php
+++ server.php
@@ -1,13 +1,4 @@
 <?php
-/*
-|--------------------------------------------------------------------------
-| Server file for "php artisan serve" command
-|--------------------------------------------------------------------------
-|
-| Adapted from https://github.com/laravel/laravel/blob/master/server.php (2014-05-14)
-| by Allan Freitas (allanfreitasci@gmail.com)
-| Github/Twitter @allanfreitas
-*/
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
@@ -15,7 +6,7 @@ $uri = urldecode($uri);
 
 $paths = require __DIR__.'/bootstrap/paths.php';
 
-$requested = __DIR__.$uri;
+$requested = $paths['public'].$uri;
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
@@ -25,4 +16,4 @@ if ($uri !== '/' and file_exists($requested))
     return false;
 }
 
-require_once 'index.php';
+require_once $paths['public'].'/index.php';
