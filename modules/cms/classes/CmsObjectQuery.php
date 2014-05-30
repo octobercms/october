c74ff59d29ec0bf61d0f94dd244d88a437597bc7
Sam Georges <sam@daftspunk.com>
2014-05-30 22:05:00 +1000

    Forgot my hat

--- modules/cms/classes/CmsObjectQuery.php
+++ modules/cms/classes/CmsObjectQuery.php
@@ -70,9 +70,9 @@ class CmsObjectQuery
     public function find($fileName)
     {
         if ($this->useCache)
-            return forward_static_call($this->cmsObject, 'loadCached', $this->theme, $fileName);
+            return forward_static_call([$this->cmsObject, 'loadCached'], $this->theme, $fileName);
         else
-            return forward_static_call($this->cmsObject, 'load', $this->theme, $fileName);
+            return forward_static_call([$this->cmsObject, 'load'], $this->theme, $fileName);
     }
 
     /**
