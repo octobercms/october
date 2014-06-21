6fcc845bf901b64fa714343109c37a6159648c08
Sam Georges <sam@daftspunk.com>
2014-06-21 13:54:47 +1000

    Create custom plugin for handsontable

--- modules/backend/formwidgets/datagrid/assets/js/datagrid.js
+++ modules/backend/formwidgets/datagrid/assets/js/datagrid.js
@@ -22,7 +22,7 @@
         this.options   = options
         this.$el       = $(element)
 
-        this.staticWidths = this.calculateColumnWidths()
+        this.staticWidths = this.options.columnWidths
 
         // Init
         var handsontableOptions = {
@@ -31,10 +31,24 @@
                 return self.staticWidths[columnIndex]
             },
             columns: this.options.columns,
-            startRows: this.options.startRows
+            startRows: this.options.startRows,
+            fillHandle: false,
+            // rowHeaders: true,
+            // manualColumnMove: true,
+            // manualRowMove: true,
+            removeRowPlugin: true
         }
 
+
         this.$el.handsontable(handsontableOptions)
+
+        this.staticWidths = this.calculateColumnWidths()
+        self.$el.handsontable('render')
+
+        $(window).on('oc.updateUi', function(){
+            self.staticWidths = self.calculateColumnWidths()
+            self.$el.handsontable('render')
+        })
 
         $(window).on('resize', function(){
             self.staticWidths = self.calculateColumnWidths()
@@ -54,7 +68,12 @@
             totalWidth = this.$el.innerWidth(),
             usedWidth = 0,
             unsetWidthCounts = 0,
-            staticWidths = []
+            staticWidths = [],
+            headerOffsetWidth = 0
+
+        this.$el.find('colgroup > .rowHeader').each(function(){
+            headerOffsetWidth += $(this).width()
+        })
 
         $.each(widths, function() {
             var width = parseInt(this)
@@ -66,7 +85,7 @@
             if (this > 0) {
                 staticWidths.push(this)
             } else {
-                var remainingWidth = ((totalWidth - usedWidth) / unsetWidthCounts) - 2
+                var remainingWidth = ((totalWidth - headerOffsetWidth - usedWidth) / unsetWidthCounts) - 2
                 staticWidths.push(Math.max(remainingWidth, 100))
             }
         })
@@ -111,3 +130,83 @@
     })
 
 }(window.jQuery);
+
+
+/*
+ * Custom plugin for handsontable
+ */
+
+
+(function (Handsontable, $) {
+    "use strict";
+
+    function init() {
+        var instance = this;
+
+        var pluginEnabled = !!(instance.getSettings().removeRowPlugin);
+
+        if (pluginEnabled) {
+            bindMouseEvents();
+            instance.rootElement.addClass('htControlPanel');
+        } else {
+            unbindMouseEvents();
+            instance.rootElement.removeClass('htControlPanel');
+        }
+
+        function bindMouseEvents() {
+            instance.rootElement.on('mouseover.removeRow', 'tbody th, tbody td', function () {
+                getButton(this).show();
+            });
+            instance.rootElement.on('mouseout.removeRow', 'tbody th, tbody td', function () {
+                getButton(this).hide();
+            });
+        }
+
+        function unbindMouseEvents() {
+            instance.rootElement.off('mouseover.removeRow');
+            instance.rootElement.off('mouseout.removeRow');
+        }
+
+        function getButton(td) {
+            return $(td).parent('tr').find('th.htControlPanel').eq(0).find('.close');
+        }
+    }
+
+
+    Handsontable.PluginHooks.add('beforeInitWalkontable', function (walkontableConfig) {
+        var instance = this;
+
+        /**
+         * rowHeaders is a function, so to alter the actual value we need to alter the result returned by this function
+         */
+        var baseRowHeaders = walkontableConfig.rowHeaders;
+        walkontableConfig.rowHeaders = function () {
+            var pluginEnabled = Boolean(instance.getSettings().removeRowPlugin);
+
+            var newRowHeader = function (row, elem) {
+
+                var $el = $(elem).empty().addClass('htNoFrame htControlPanel'),
+                    $btn;
+
+                if (row > -1) {
+                    $btn = $('<button />').addClass('close').html('&times;').hide();
+                    $el.append($btn);
+
+                    $btn.on('mouseup', function () {
+                        instance.alter("remove_row", row);
+                    });
+                }
+            };
+
+            return pluginEnabled ? Array.prototype.concat.call([], newRowHeader, baseRowHeaders()) : baseRowHeaders();
+        };
+    });
+
+    Handsontable.PluginHooks.add('beforeInit', function () {
+        init.call(this)
+    });
+
+    Handsontable.PluginHooks.add('afterUpdateSettings', function () {
+        init.call(this)
+    });
+})(Handsontable, jQuery);
