04bbad320fb033e2e0ebde001b58a469ff6164ec
Sam Georges <sam@daftspunk.com>
2014-07-12 13:16:16 +1000

    Fixes #347, Fixes #401 - Allow user routes to be registered before the CMS

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -67,8 +67,8 @@ if (!isset($unitTesting) || !$unitTesting) {
 	header('Cache-Control: pre-check=0, post-check=0, max-age=0, max-stale = 0', false);    // HTTP/1.1
 	header('Pragma: public');
 	header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');                                       // Date in the past  
-	header('Expires: 0', false); 
-	header('Last-Modified: '.gmdate('D, d M Y H:i:s') . ' GMT');
+	header('Expires: 0', false);
+	header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
 	header('Pragma: no-cache');
 }
 
--- modules/cms/routes.php
+++ modules/cms/routes.php
@@ -1,12 +1,19 @@
 <?php
 
 /*
- * Combine JavaScript and StyleSheet assets
+ * Register CMS routes before all user routes.
  */
-Route::any('combine/{file}', 'Cms\Classes\Controller@combine');
+App::before(function($request) {
 
-/*
- * The CMS module intercepts all URLs that were not 
- * handled by the back-end modules.
- */
-Route::any('{slug}', 'Cms\Classes\Controller@run')->where('slug', '(.*)?');
+    /*
+     * Combine JavaScript and StyleSheet assets
+     */
+    Route::any('combine/{file}', 'Cms\Classes\Controller@combine');
+
+    /*
+     * The CMS module intercepts all URLs that were not 
+     * handled by the back-end modules.
+     */
+    Route::any('{slug}', 'Cms\Classes\Controller@run')->where('slug', '(.*)?');
+
+});
