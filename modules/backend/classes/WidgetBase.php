10d30fcd9efc7f50a03f89e0065c7233604451c4
flynsarmy <flynsarmy@gmail.com>
2014-07-27 22:04:24 +1000

    Spaces, not tabs

--- modules/backend/classes/WidgetBase.php
+++ modules/backend/classes/WidgetBase.php
@@ -131,7 +131,7 @@ abstract class WidgetBase
         if ($suffix !== null)
             $id .= '-' . $suffix;
 
-	$id = str_replace(['[', ']'], '__', $id);
+        $id = str_replace(['[', ']'], '__', $id);
 
         return $id;
     }
