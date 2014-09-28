8ca3483863f7088e1662effc2be810111658b78d
Sam Georges <sam@daftspunk.com>
2014-09-28 10:29:49 +1000

    Refs #653 - Move sessions table to system

---
+++ modules/system/database/migrations/2014_10_01_000013_Db_System_Sessions.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DbSystemSessions extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function(Blueprint $table)
+        {
+            $table->string('id')->unique();
+            $table->text('payload')->nullable();
+            $table->integer('last_activity')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+
+}
