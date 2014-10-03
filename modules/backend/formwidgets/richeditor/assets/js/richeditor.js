fee5925246aeb31291c962a4e0e8ab19d580e5b5
Sam Georges <sam@daftspunk.com>
2014-10-03 17:59:18 +1000

    Revert image plugin until completed

--- modules/backend/formwidgets/richeditor/assets/js/richeditor.js
+++ modules/backend/formwidgets/richeditor/assets/js/richeditor.js
@@ -72,8 +72,10 @@
             redactorOptions.fullpage = true
         }
 
-        redactorOptions.plugins = ['cleanup', 'fullscreen', 'figure', 'image', 'quote', 'table']
-        redactorOptions.buttons = ['formatting', 'bold', 'italic', 'unorderedlist', 'orderedlist', 'link', 'horizontalrule', 'html'],
+        // redactorOptions.plugins = ['cleanup', 'fullscreen', 'figure', 'image', 'quote', 'table']
+        // redactorOptions.buttons = ['formatting', 'bold', 'italic', 'unorderedlist', 'orderedlist', 'link', 'horizontalrule', 'html'],
+        redactorOptions.plugins = ['cleanup', 'fullscreen', 'figure', 'quote', 'table']
+        redactorOptions.buttons = ['formatting', 'bold', 'italic', 'unorderedlist', 'orderedlist', 'image', 'link', 'horizontalrule', 'html'],
 
         this.$textarea.redactor(redactorOptions)
     }
