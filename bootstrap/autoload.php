bf515bfa9ad54321bbcfcf467793770913fabf78
Stefan Talen <s.r.talen@gmail.com>
2014-10-10 21:58:29 +0200

    Updating bootstrap folder to PSR-2

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -27,9 +27,8 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-if (file_exists($compiled = __DIR__.'/compiled.php'))
-{
-	require $compiled;
+if (file_exists($compiled = __DIR__.'/compiled.php')) {
+    require $compiled;
 }
 
 /*
