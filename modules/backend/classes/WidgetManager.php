f4487076b7bfea1281b285def07d2c246bb0fcb4
Samuel Georges <sam@daftspunk.com>
2015-01-05 13:12:48 +1100

    Remove some legacy code, no need to pipe WidgetMaker thru WidgetManager

--- modules/backend/classes/WidgetManager.php
+++ modules/backend/classes/WidgetManager.php
@@ -2,12 +2,10 @@
 
 use Str;
 use File;
-use Lang;
 use Closure;
 use October\Rain\Support\Yaml;
 use Illuminate\Container\Container;
 use System\Classes\PluginManager;
-use System\Classes\SystemException;
 
 /**
  * Widget manager
@@ -55,34 +53,6 @@ class WidgetManager
     protected function init()
     {
         $this->pluginManager = PluginManager::instance();
-    }
-
-    /**
-     * Makes a widget object with configuration set.
-     * @param string $className A widget class name.
-     * @param Controller $controller The Backend controller that spawned this widget.
-     * @param array $configuration Configuration values.
-     * @return WidgetBase The widget object.
-     */
-    public function makeWidget($className, $controller = null, $configuration = null)
-    {
-        /*
-         * Build configuration
-         */
-        if ($configuration === null) {
-            $configuration = [];
-        }
-
-        /*
-         * Create widget object
-         */
-        if (!class_exists($className)) {
-            throw new SystemException(Lang::get('backend::lang.widget.not_registered', [
-                'name' => $className
-            ]));
-        }
-
-        return new $className($controller, $configuration);
     }
 
     //
