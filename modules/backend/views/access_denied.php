d2d8d4df2e9a026d0d1ff5a063a09c3f760597b8
Marius van Witzenburg <marius@kitara.nl>
2015-01-26 22:16:00 +0100

    Replaced short open tags

--- modules/backend/views/access_denied.php
+++ modules/backend/views/access_denied.php
@@ -2,15 +2,15 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title><?= Lang::get('backend::lang.page.access_denied.label') ?></title>
-        <link href="<?= URL::to('/modules/system/assets/vendor/font-autumn/css/font-autumn.css') ?>" rel="stylesheet">
-        <link href="<?= URL::to('/modules/system/assets/css/styles.css') ?>" rel="stylesheet">
+        <title><?php echo Lang::get('backend::lang.page.access_denied.label') ?></title>
+        <link href="<?php echo URL::to('/modules/system/assets/vendor/font-autumn/css/font-autumn.css') ?>" rel="stylesheet">
+        <link href="<?php echo URL::to('/modules/system/assets/css/styles.css') ?>" rel="stylesheet">
     </head>
     <body>
     <div class="container">
-        <h1><i class="icon-lock warning"></i> <?= Lang::get('backend::lang.page.access_denied.label') ?></h1>
-        <p class="lead"><?= Lang::get('backend::lang.page.access_denied.help') ?></p>
-        <a href="<?= Backend::url('/') ?>"><?= Lang::get('backend::lang.page.access_denied.cms_link') ?></a>
+        <h1><i class="icon-lock warning"></i> <?php echo Lang::get('backend::lang.page.access_denied.label') ?></h1>
+        <p class="lead"><?php echo Lang::get('backend::lang.page.access_denied.help') ?></p>
+        <a href="<?php echo Backend::url('/') ?>"><?php echo Lang::get('backend::lang.page.access_denied.cms_link') ?></a>
     </div>
     </body>
 </html>
--- modules/cms/views/error.php
+++ modules/cms/views/error.php
@@ -2,14 +2,14 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title><?= Lang::get('cms::lang.page.custom_error.label') ?></title>
-        <link href="<?= URL::to('/modules/system/assets/vendor/font-autumn/css/font-autumn.css') ?>" rel="stylesheet">
-        <link href="<?= URL::to('/modules/system/assets/css/styles.css') ?>" rel="stylesheet">
+        <title><?php echo Lang::get('cms::lang.page.custom_error.label') ?></title>
+        <link href="<?php echo URL::to('/modules/system/assets/vendor/font-autumn/css/font-autumn.css') ?>" rel="stylesheet">
+        <link href="<?php echo URL::to('/modules/system/assets/css/styles.css') ?>" rel="stylesheet">
     </head>
     <body>
         <div class="container">
-            <h1><i class="icon-power-off warning"></i> <?= Lang::get('cms::lang.page.custom_error.label') ?></h1>
-            <p class="lead"><?= Lang::get('cms::lang.page.custom_error.help') ?></p>
+            <h1><i class="icon-power-off warning"></i> <?php echo Lang::get('cms::lang.page.custom_error.label') ?></h1>
+            <p class="lead"><?php echo Lang::get('cms::lang.page.custom_error.help') ?></p>
         </div>
     </body>
 </html>
