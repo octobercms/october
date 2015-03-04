561e969d2d8836a56b49b35af8067edfed2d9758
Samuel Georges <sam@daftspunk.com>
2015-03-04 18:23:02 +1100

    Extract controls to their own stylesheet (So they can be used elsewhere; namely documentation) Add indicator-center and clean up load indicator Fancy layout should live with the other layouts, it isn't really a control

--- modules/backend/ServiceProvider.php
+++ modules/backend/ServiceProvider.php
@@ -166,6 +166,7 @@ class ServiceProvider extends ModuleServiceProvider
          * Register asset bundles
          */
         CombineAssets::registerCallback(function($combiner) {
+            $combiner->registerBundle('~/modules/backend/assets/less/controls.less');
             $combiner->registerBundle('~/modules/backend/assets/less/october.less');
             $combiner->registerBundle('~/modules/backend/assets/js/october.js');
             $combiner->registerBundle('~/modules/backend/assets/js/vendor/vendor.js');
--- modules/backend/assets/js/october-min.js
+++ modules/backend/assets/js/october-min.js
@@ -939,8 +939,7 @@ this.hide()
 var indicator=$('<div class="loading-indicator"></div>')
 indicator.append($('<div></div>').text(this.options.text))
 indicator.append($('<span></span>'))
-if(this.options.opaque==undefined)
-indicator.addClass('transparent')
+if(this.options.opaque!==undefined){indicator.addClass('is-opaque')}
 this.$el.prepend(indicator)
 this.$el.addClass('in-progress')
 this.tally++}
