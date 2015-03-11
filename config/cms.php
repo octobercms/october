b3dcce6d2ac7346c291c3e8bcc05f45ff528f396
Pásztor Gábor <gpasztor87@gmail.com>
2015-03-11 20:30:41 +0100

    Updates on Brand Settings

--- config/cms.php
+++ config/cms.php
@@ -1,6 +1,6 @@
 <?php
 
-return array(
+return [
 
     /*
     |--------------------------------------------------------------------------
@@ -250,4 +250,4 @@ return array(
 
     'defaultMask' => ['file' => null, 'folder' => null],
 
-);
+];
