8d93c9ffabca25950ca63dab40dbd014c1671952
Samuel Georges <sam@daftspunk.com>
2015-03-25 19:33:00 +1100

    List query is now AND where instead of OR where Refs #775 to support soft deletes

--- modules/backend/widgets/Lists.php
+++ modules/backend/widgets/Lists.php
@@ -432,7 +432,7 @@ class Lists extends WidgetBase
          * Apply a supplied search term for primary columns
          */
         if (count($primarySearchable) > 0) {
-            $query->orWhere(function ($innerQuery) use ($primarySearchable) {
+            $query->where(function ($innerQuery) use ($primarySearchable) {
                 $innerQuery->searchWhere($this->searchTerm, $primarySearchable);
             });
         }
