a16d806e9384cda3e60910a1db04d6f2ca2f29f9
Alexander Romanov <romanov-acc@users.noreply.github.com>
2015-04-13 04:45:00 +0300

    Update lang.php

--- modules/system/lang/ru/lang.php
+++ modules/system/lang/ru/lang.php
@@ -23,7 +23,7 @@ return [
         'ru' => 'Russian',
         'se' => 'Swedish',
         'sk' => 'Slovak (Slovakia)',
-        'tr' => 'Turkish'
+        'tr' => 'Turkish',
         'nb-no' => 'Norwegian (Bokmål)'
     ],
     'directory' => [
