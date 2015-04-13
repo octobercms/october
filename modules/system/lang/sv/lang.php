25f61702872a2973b7cfdf5c40b18fc02a6adf39
Samuel Georges <sam@daftspunk.com>
2015-04-13 15:09:09 +1000

    Fixes #1066

--- modules/system/lang/sv/lang.php
+++ modules/system/lang/sv/lang.php
@@ -23,7 +23,7 @@ return [
         'ru' => 'Russian',
         'se' => 'Swedish',
         'sk' => 'Slovak (Slovakia)',
-        'tr' => 'Turkish'
+        'tr' => 'Turkish',
         'nb-no' => 'Norska (Bokmål)'
     ],
     'directory' => [
