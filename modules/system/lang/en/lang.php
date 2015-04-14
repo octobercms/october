d1cc78a09871dadd79504d9f21899b508e67ebf9
刘星 <liuxingmatt@gmail.com>
2015-04-14 10:17:02 +0800

    Add setting in English version

--- modules/system/lang/en/lang.php
+++ modules/system/lang/en/lang.php
@@ -23,7 +23,8 @@ return [
         'ru' => 'Russian',
         'se' => 'Swedish',
         'sk' => 'Slovak (Slovakia)',
-        'tr' => 'Turkish'
+        'tr' => 'Turkish',
+        'zh-cn' => 'Chinese (China)'
     ],
     'directory' => [
         'create_fail' => 'Cannot create directory: :name'
