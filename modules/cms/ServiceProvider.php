85bdabe78339157d901bc410810d03feefce1288
Dave Shoreman <code@dsdev.io>
2015-04-22 22:23:30 +0100

    Update cms module for new order functionality
    Since sideMenu items are now sortable, any sideMenu items that don't
    have an order applied will get sorted however PHP decides to do so.
    This adds the order parameter to all cms menu items to retain the
    original order of the side menu items.

--- modules/cms/ServiceProvider.php
+++ modules/cms/ServiceProvider.php
@@ -39,6 +39,7 @@ class ServiceProvider extends ModuleServiceProvider
                             'label'        => 'cms::lang.page.menu_label',
                             'icon'         => 'icon-copy',
                             'url'          => 'javascript:;',
+                            'order'        => 100,
                             'attributes'   => ['data-menu-item' => 'pages'],
                             'permissions'  => ['cms.manage_pages'],
                             'counterLabel' => 'cms::lang.page.unsaved_label'
@@ -47,6 +48,7 @@ class ServiceProvider extends ModuleServiceProvider
                             'label'        => 'cms::lang.partial.menu_label',
                             'icon'         => 'icon-tags',
                             'url'          => 'javascript:;',
+                            'order'        => 200,
                             'attributes'   => ['data-menu-item' => 'partials'],
                             'permissions'  => ['cms.manage_partials'],
                             'counterLabel' => 'cms::lang.partial.unsaved_label'
@@ -55,6 +57,7 @@ class ServiceProvider extends ModuleServiceProvider
                             'label'        => 'cms::lang.layout.menu_label',
                             'icon'         => 'icon-th-large',
                             'url'          => 'javascript:;',
+                            'order'        => 300,
                             'attributes'   => ['data-menu-item' => 'layouts'],
                             'permissions'  => ['cms.manage_layouts'],
                             'counterLabel' => 'cms::lang.layout.unsaved_label'
@@ -63,6 +66,7 @@ class ServiceProvider extends ModuleServiceProvider
                             'label'        => 'cms::lang.content.menu_label',
                             'icon'         => 'icon-file-text-o',
                             'url'          => 'javascript:;',
+                            'order'        => 400,
                             'attributes'   => ['data-menu-item' => 'content'],
                             'permissions'  => ['cms.manage_content'],
                             'counterLabel' => 'cms::lang.content.unsaved_label'
@@ -71,6 +75,7 @@ class ServiceProvider extends ModuleServiceProvider
                             'label'        => 'cms::lang.asset.menu_label',
                             'icon'         => 'icon-picture-o',
                             'url'          => 'javascript:;',
+                            'order'        => 500,
                             'attributes'   => ['data-menu-item' => 'assets'],
                             'permissions'  => ['cms.manage_assets'],
                             'counterLabel' => 'cms::lang.asset.unsaved_label'
@@ -79,6 +84,7 @@ class ServiceProvider extends ModuleServiceProvider
                             'label'       => 'cms::lang.component.menu_label',
                             'icon'        => 'icon-puzzle-piece',
                             'url'         => 'javascript:;',
+                            'order'        => 600,
                             'attributes'  => ['data-menu-item' => 'components'],
                             'permissions' => ['cms.manage_pages', 'cms.manage_layouts', 'cms.manage_partials']
                         ]
