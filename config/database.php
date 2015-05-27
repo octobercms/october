436da25315b92e2c7b0418c11defa8970864a70f
Joel Villalobos Batista <jovib29@gmail.com>
2015-05-27 07:52:18 -0500

    fix indentation

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
+            'host'      => env('DB_HOST', 'localhost'),
             'port'     => '',
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', 'database'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
@@ -66,11 +66,11 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-			'host'      => env('DB_HOST', 'localhost'),
+            'host'      => env('DB_HOST', 'localhost'),
             'port'     => '',
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', 'database'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
@@ -78,11 +78,11 @@ return [
 
         'sqlsrv' => [
             'driver'   => 'sqlsrv',
-			'host'      => env('DB_HOST', 'localhost'),
+            'host'      => env('DB_HOST', 'localhost'),
             'port'     => '',
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', 'database'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
             'prefix'   => '',
         ],
 
