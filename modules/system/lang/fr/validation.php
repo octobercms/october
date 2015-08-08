20fdc0bb4f32b613e98291ed46be45fc30de92bd
Raphaël M <contact@galixte.com>
2015-08-08 18:43:44 +0200

    Update of FRENCH translation for OctoberCMS

--- modules/system/lang/fr/validation.php
+++ modules/system/lang/fr/validation.php
@@ -46,7 +46,7 @@ return array(
         "array"   => 'Le champ :attribute ne peut pas être supérieure à :max objets.',
     ),
     "mimes"            => 'Le champ :attribute doit être un fichier de type : :values.',
-    "extensions"       => 'Le champ :attribute doit être une extension de : :values.",
+    "extensions"       => 'Le champ :attribute doit être une extension de : :values.',
     "min"              => array(
         "numeric" => 'Le champ :attribute doit être au minimum de :min.',
         "file"    => 'Le champ :attribute doit être au minimum de :min kilooctets.',
