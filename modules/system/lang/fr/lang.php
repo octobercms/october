95c6112c5e0507541b008213bb81271ed46ead0c
Jérémy GAULIN <jeremy@bnb.re>
2015-08-13 10:38:43 +0200

    https://github.com/octobercms/october/pull/1363/files#r36929168

--- modules/system/lang/fr/lang.php
+++ modules/system/lang/fr/lang.php
@@ -185,8 +185,8 @@ return [
         'test_success' => 'Le message de test a été envoyé avec succès.',
         'return' => 'Retour à la liste des modèles.',
         'test_confirm' => 'Un message de test sera envoyé à :email. Continuer ?',
-        'saving' => 'Sauvegarde du modèle en cours...',
-        'sending' => 'Envoi du message de test en cours...',
+        'saving' => 'Sauvegarde du modèle en cours…',
+        'sending' => 'Envoi du message de test en cours…',
     ],
     'install' => [
         'project_label' => 'Attacher un projet',
