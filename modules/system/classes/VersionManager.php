6f414fe6114064547c6ffd89ff58143a862f4cca
Samuel Georges <sam@daftspunk.com>
2015-08-15 10:00:11 +1000

    Default direction is ascending

--- modules/system/classes/VersionManager.php
+++ modules/system/classes/VersionManager.php
@@ -390,7 +390,7 @@ class VersionManager
             return $this->databaseHistory[$code];
         }
 
-        $historyInfo = Db::table('system_plugin_history')->where('code', $code)->orderBy('id','ASC')->get();
+        $historyInfo = Db::table('system_plugin_history')->where('code', $code)->orderBy('id')->get();
 
         return $this->databaseHistory[$code] = $historyInfo;
     }
