07c8b23c7d38099bfa3508015e5ef00c9bfb0be8
Szabó Gergő <gergo85@gmail.com>
2015-08-22 12:09:56 +0200

    Big updated for Hungarian language

---
+++ modules/system/lang/hu/client.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Client-side Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | These are messages made available to the client browser via JavaScript.
+    | To compile this file run: php artisan october:util compile lang
+    |
+    */
+
+    'markdowneditor' => [
+        'formatting' => 'Forráskód',
+        'quote' => 'Idézet',
+        'code' => 'Kód',
+        'header1' => 'Főcím 1',
+        'header2' => 'Főcím 2',
+        'header3' => 'Főcím 3',
+        'header4' => 'Főcím 4',
+        'header5' => 'Főcím 5',
+        'header6' => 'Főcím 6',
+        'bold' => 'Félkövér',
+        'italic' => 'Dölt',
+        'unorderedlist' => 'Rendezett lista',
+        'orderedlist' => 'Számozott lista',
+        'video' => 'Videó',
+        'image' => 'Kép',
+        'link' => 'Hivatkozás',
+        'horizontalrule' => 'Vonal beszúrása',
+        'fullscreen' => 'Teljes képernyő',
+        'preview' => 'Előnézet'
+    ],
+
+    'mediamanager' => [
+        'insert_link' => 'Hivatkozás beszúrása',
+        'insert_image' => 'Kép beszúrása',
+        'insert_video' => 'Videó beszúrása',
+        'insert_audio' => 'Audió beszúrása',
+        'invalid_file_empty_insert' => 'Hivatkozás készítéséhez jelöljön ki egy szövegrészt.',
+        'invalid_file_single_insert' => 'Kérjük jelöljön ki egy fájlt.',
+        'invalid_image_empty_insert' => 'Válasszon ki legalább egy képet a beszúráshoz.',
+        'invalid_video_empty_insert' => 'Válasszon ki legalább egy videót a beszúráshoz.',
+        'invalid_audio_empty_insert' => 'Válasszon ki legalább egy audiót a beszúráshoz.'
+    ],
+
+    'alert' => [
+        'confirm_button_text' => 'OK',
+        'cancel_button_text' => 'Mégsem'
+    ]
+
+];
