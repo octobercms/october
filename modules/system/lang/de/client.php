b28e5ed9a034b86ebf8a421e9fd3f5815d926792
Marco Tesche <marco@tesche.biz>
2015-09-26 19:49:35 +0200

    German translation for client.php (system module)

---
+++ modules/system/lang/de/client.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Client-side Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | These are messages made available to the client browser via JavaScript.
+    | To compile this file run: php artisan october:util compile lang
+    |
+    */
+
+    'markdowneditor' => [
+        'formatting' => 'Formatierung',
+        'quote' => 'Zitat',
+        'code' => 'Code',
+        'header1' => 'Überschrift 1',
+        'header2' => 'Überschrift 2',
+        'header3' => 'Überschrift 3',
+        'header4' => 'Überschrift 4',
+        'header5' => 'Überschrift 5',
+        'header6' => 'Überschrift 6',
+        'bold' => 'Fett',
+        'italic' => 'Kursiv',
+        'unorderedlist' => 'Normale Liste',
+        'orderedlist' => 'Nummerierte Liste',
+        'video' => 'Video',
+        'image' => 'Bild',
+        'link' => 'Link',
+        'horizontalrule' => 'Horizontale Linie',
+        'fullscreen' => 'Vollbild',
+        'preview' => 'Vorschau',
+    ],
+
+    'mediamanager' => [
+        'insert_link' => "Link aus Medienbibliothek",
+        'insert_image' => "Bild aus Medienbibliothek",
+        'insert_video' => "Video aus Medienbibliothek",
+        'insert_audio' => "Audio aus Medienbibliothek",
+        'invalid_file_empty_insert' => "Bitte Datei auswählen.",
+        'invalid_file_single_insert' => "Bitte nur eine Datei wählen.",
+        'invalid_image_empty_insert' => "Bitte ein Bild auswählen.",
+        'invalid_video_empty_insert' => "Bitte ein Video auswählen.",
+        'invalid_audio_empty_insert' => "Bitte eine Audiodatei auswählen.",
+    ],
+
+    'alert' => [
+        'confirm_button_text' => 'OK',
+        'cancel_button_text' => 'Abbrechen',
+    ],
+
+];
+
+
+
