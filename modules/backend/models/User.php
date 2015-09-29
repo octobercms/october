8139def4351d4216b813f98afd4c4d15a981489f
Samuel Georges <sam@daftspunk.com>
2015-09-30 06:09:51 +1000

    Bump max values to their db equivalents

--- modules/backend/models/User.php
+++ modules/backend/models/User.php
@@ -22,9 +22,9 @@ class User extends UserBase
      */
     public $rules = [
         'login' => 'required|between:2,24|unique:backend_users',
-        'email' => 'required|between:3,64|email|unique:backend_users',
-        'password' => 'required:create|between:4,64|confirmed',
-        'password_confirmation' => 'required_with:password|between:4,64'
+        'email' => 'required|between:3,255|email|unique:backend_users',
+        'password' => 'required:create|between:4,255|confirmed',
+        'password_confirmation' => 'required_with:password|between:4,255'
     ];
 
     /**
