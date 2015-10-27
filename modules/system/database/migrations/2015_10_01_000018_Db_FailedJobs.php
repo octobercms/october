1c68dad369d7e6d131639d8ee7f9ee5826869099
Thaine Rowley <trowley@oncalladvisors.com>
2015-10-27 10:37:09 -0500

    Added a check for the Failed Jobs table, in case you have already created it from laravel migration

--- modules/system/database/migrations/2015_10_01_000018_Db_FailedJobs.php
+++ modules/system/database/migrations/2015_10_01_000018_Db_FailedJobs.php
@@ -7,14 +7,18 @@ class DbFailedJobs extends Migration
 {
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
-            $table->increments('id');
-            $table->text('connection');
-            $table->text('queue');
-            $table->text('payload');
-            $table->timestamp('failed_at');
-        });
+        // Check, in case you already created the failed_jobs table from laravel migration
+        if (!Schema::hasTable('failed_jobs')) {
+
+            Schema::create('failed_jobs', function (Blueprint $table) {
+                $table->engine = 'InnoDB';
+                $table->increments('id');
+                $table->text('connection');
+                $table->text('queue');
+                $table->text('payload');
+                $table->timestamp('failed_at');
+            });
+        }
     }
 
     public function down()
