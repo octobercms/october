12972cd086692f0f91a48d9d86947601bb88db19
Samuel Georges <sam@daftspunk.com>
2015-12-19 09:53:17 +1100

    Lax the validation rules on login, email + pass

--- modules/backend/models/User.php
+++ modules/backend/models/User.php
@@ -21,8 +21,8 @@ class User extends UserBase
      * Validation rules
      */
     public $rules = [
-        'login' => 'required|between:2,24|unique:backend_users',
-        'email' => 'required|between:3,255|email|unique:backend_users',
+        'email' => 'required|between:6,255|email|unique:backend_users',
+        'login' => 'required|between:2,255|unique:backend_users',
         'password' => 'required:create|between:4,255|confirmed',
         'password_confirmation' => 'required_with:password|between:4,255'
     ];
