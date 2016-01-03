2087b575d3edaaee2de2022562bd595859060240
Samuel Georges <sam@daftspunk.com>
2016-01-04 07:52:57 +1100

    Roll back compiled path, which is still defined in the storage folder

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -47,7 +47,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$compiledPath = __DIR__.'/cache/compiled.php';
+$compiledPath = __DIR__.'/../storage/framework/compiled.php';
 
 if (file_exists($compiledPath)) {
     require $compiledPath;
