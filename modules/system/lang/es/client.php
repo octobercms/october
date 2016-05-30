60602c5455e81a0e5814bfd8c40c4b2c82e5b003
Lucas Martín <codedeep@users.noreply.github.com>
2016-05-31 00:19:15 +0200

    Added untranslated strings to spanish (#2073)
    * Added untranslated strings to spanish
    
    Added untranslated strings to spanish and correct commas and others
    
    * Added untranslated strings to spanish
    
    Added untranslated strings to spanish and correct commas
    
    * Update lang.php
    
    * Update client.php

--- modules/system/lang/es/client.php
+++ modules/system/lang/es/client.php
@@ -31,7 +31,7 @@ return [
         'link' => 'Vínculo',
         'horizontalrule' => 'Insertar Regla Horizontal',
         'fullscreen' => 'Pantalla completa',
-        'preview' => 'Previsualizar',
+        'preview' => 'Previsualizar'
     ],
 
     'mediamanager' => [
@@ -48,7 +48,45 @@ return [
 
     'alert' => [
         'confirm_button_text' => 'OK',
-        'cancel_button_text' => 'Cancelar',
+        'cancel_button_text' => 'Cancelar'
     ],
 
+    'datepicker' => [
+        'previousMonth' => 'Mes Anterior',
+        'nextMonth' => 'Mes Siguiente',
+        'months' => ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        'weekdays' => ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'],
+        'weekdaysShort' => ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab']
+    ],
+
+    'filter' => [
+        'group' => [
+            'all' => 'todos'
+        ],
+        'dates' => [
+            'all' => 'todos',
+            'filter_button_text' => 'Filtro',
+            'reset_button_text'  => 'Restablecer',
+            'date_placeholder' => 'Fecha',
+            'after_placeholder' => 'Despues',
+            'before_placeholder' => 'Antes'
+        ]
+    ],
+
+    'eventlog' => [
+        'show_stacktrace' => 'Mostrar el seguimiento de la pila',
+        'hide_stacktrace' => 'Ocultar el seguimiento de la pila',
+        'tabs' => [
+            'formatted' => 'Formateado',
+            'raw' => 'Sin formato',
+        ],
+        'editor' => [
+            'title' => 'Seleccione el editor de código fuente a usar',
+            'description' => 'Su entorno de sistema operativo debe estar configurado para escuchar a uno de estos esquemas de URL.',
+            'openWith' => 'Abrir con',
+            'rememberChoice' => 'Recuerde que la opción seleccionada para esta sesión del navegador',
+            'open' => 'Abrir',
+            'cancel' => 'Cancelar'
+        ]
+    ]
 ];
