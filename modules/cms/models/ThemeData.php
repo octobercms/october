6ca828a1fae77b4c39a4ca985f289e3c613783f7
Samuel Georges <sam@daftspunk.com>
2016-06-16 08:01:05 +1000

    Always return an active theme and data

--- modules/cms/models/ThemeData.php
+++ modules/cms/models/ThemeData.php
@@ -4,6 +4,7 @@ use Lang;
 use Model;
 use Cms\Classes\Theme as CmsTheme;
 use System\Classes\CombineAssets;
+use Exception;
 
 /**
  * Customization data used by a theme
@@ -82,7 +83,14 @@ class ThemeData extends Model
             return $themeData;
         }
 
-        $themeData = ThemeData::firstOrCreate(['theme' => $dirName]);
+        try {
+            $themeData = ThemeData::firstOrCreate(['theme' => $dirName]);
+        }
+        catch (Exception $ex) {
+            // Database failed
+            $themeData = new ThemeData(['theme' => $dirName]);
+        }
+
         return self::$instances[$dirName] = $themeData;
     }
 
@@ -162,8 +170,9 @@ class ThemeData extends Model
      */
     public function getFormFields()
     {
-        if (!$theme = CmsTheme::load($this->theme))
+        if (!$theme = CmsTheme::load($this->theme)) {
             throw new Exception(Lang::get('Unable to find theme with name :name', $this->theme));
+        }
 
         $config = $theme->getConfigArray('form');
 
