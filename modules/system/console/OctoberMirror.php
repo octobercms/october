d3b2809866d135b4d88f032929736602298b2210
Samuel Georges <sam@daftspunk.com>
2016-06-17 07:09:14 +1000

    htaccess now resembles october:mirror command Added 'resources' as a potential asset storage directory name

--- modules/system/console/OctoberMirror.php
+++ modules/system/console/OctoberMirror.php
@@ -41,18 +41,29 @@ class OctoberMirror extends Command
 
     protected $wildcards = [
         'modules/*/assets',
+        'modules/*/resources',
         'modules/*/behaviors/*/assets',
+        'modules/*/behaviors/*/resources',
         'modules/*/widgets/*/assets',
+        'modules/*/widgets/*/resources',
         'modules/*/formwidgets/*/assets',
+        'modules/*/formwidgets/*/resources',
         'modules/*/reportwidgets/*/assets',
+        'modules/*/reportwidgets/*/resources',
 
         'plugins/*/*/assets',
+        'plugins/*/*/resources',
         'plugins/*/*/behaviors/*/assets',
+        'plugins/*/*/behaviors/*/resources',
         'plugins/*/*/reportwidgets/*/assets',
+        'plugins/*/*/reportwidgets/*/resources',
         'plugins/*/*/formwidgets/*/assets',
+        'plugins/*/*/formwidgets/*/resources',
         'plugins/*/*/widgets/*/assets',
+        'plugins/*/*/widgets/*/resources',
 
         'themes/*/assets',
+        'themes/*/resources',
     ];
 
     protected $destinationPath;
