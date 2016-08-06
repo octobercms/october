db7917a879ca6f15a2df47f443248b15aa0031da
coderwen <coderwen@gmail.com>
2016-08-06 09:29:14 +0800

    Add missing Chinese translation

--- modules/system/lang/zh-cn/lang.php
+++ modules/system/lang/zh-cn/lang.php
@@ -29,7 +29,7 @@ return [
         'sk' => 'Slovak (Slovakia)',
         'tr' => 'Turkish',
         'zh-cn' => '简体中文',
-        'zh-tw' => '繁體中文',
+        'zh-tw' => 'Chinese (Taiwan)',
         'el' => 'Greek'
     ],
     'directory' => [
@@ -301,7 +301,7 @@ return [
         'referer' => '来源',
         'url' => 'URL',
         'status_code' => '状态',
-        'preview_title' => '预览事件日志'
+        'preview_title'=> '预览事件日志'
     ],
     'permissions' => [
         'name' => '系统',
@@ -311,6 +311,8 @@ return [
         'manage_mail_templates' => '管理邮件模板',
         'manage_mail_settings' => '管理邮件设置',
         'manage_other_administrators' => '管理其他管理员',
+        'manage_preferences' => '管理后台偏好设置',
+        'manage_editor' => '管理代码编辑器偏好设置',
         'view_the_dashboard' => '查看仪表盘',
         'manage_branding' => '自定义后台'
     ]
