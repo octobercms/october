89e390edcc43246842ee8f7786162eef7cf67e3b
Samuel Georges <sam@daftspunk.com>
2016-08-26 20:01:04 +1000

    Popup will now shake when an error occurs Improve memory management / garbage collection

--- modules/system/assets/ui/storm-min.js
+++ modules/system/assets/ui/storm-min.js
@@ -3393,8 +3393,8 @@ $.fn.ocPopover.Constructor=Popover
 $.fn.ocPopover.noConflict=function(){$.fn.ocPopover=old
 return this}
 $(document).on('click','[data-control=popover]',function(e){$(this).ocPopover()
-return false;})}(window.jQuery);+function($){"use strict";var Popup=function(element,options){var self=this
-this.options=options
+return false;})}(window.jQuery);+function($){"use strict";var Base=$.oc.foundation.base,BaseProto=Base.prototype
+var Popup=function(element,options){this.options=options
 this.$el=$(element)
 this.$container=null
 this.$modal=null
@@ -3406,28 +3406,17 @@ this.$container=this.createPopupContainer()
 this.$content=this.$container.find('.modal-content:first')
 this.$dialog=this.$container.find('.modal-dialog:first')
 this.$modal=this.$container.modal({show:false,backdrop:false,keyboard:this.options.keyboard})
-this.$container.data('oc.popup',this)
-this.$modal.on('hide.bs.modal',function(){self.triggerEvent('hide.oc.popup')
-self.isOpen=false
-self.setBackdrop(false)})
-this.$modal.on('hidden.bs.modal',function(){self.triggerEvent('hidden.oc.popup')
-self.$container.remove()
-self.$el.data('oc.popup',null)
-$(document.body).removeClass('modal-open')})
-this.$modal.on('show.bs.modal',function(){self.isOpen=true
-self.setBackdrop(true)
-$(document.body).addClass('modal-open')})
-this.$modal.on('shown.bs.modal',function(){self.triggerEvent('shown.oc.popup')})
-this.$modal.on('close.oc.popup',function(){self.hide()
-return false})
+$.oc.foundation.controlUtils.markDisposable(element)
+Base.call(this)
+this.initEvents()
 this.init()}
+Popup.prototype=Object.create(BaseProto)
+Popup.prototype.constructor=Popup
 Popup.DEFAULTS={ajax:null,handler:null,keyboard:true,extraData:{},content:null,size:null,adaptiveHeight:false,zIndex:null}
 Popup.prototype.init=function(){var self=this
-if(self.isOpen)
-return
+if(self.isOpen)return
 this.setBackdrop(true)
-if(!this.options.content)
-this.setLoading(true)
+if(!this.options.content){this.setLoading(true)}
 if(this.options.handler){this.$el.request(this.options.handler,{data:paramToObj('data-extra-data',this.options.extraData),success:function(data,textStatus,jqXHR){this.success(data,textStatus,jqXHR).done(function(){self.setContent(data.result)
 $(window).trigger('ajaxUpdateComplete',[this,data,textStatus,jqXHR])
 self.triggerEvent('popupComplete')
@@ -3437,6 +3426,36 @@ self.triggerEvent('error.oc.popup')})}})}
 else if(this.options.ajax){$.ajax({url:this.options.ajax,data:paramToObj('data-extra-data',this.options.extraData),success:function(data){self.setContent(data)},cache:false})}
 else if(this.options.content){var content=typeof this.options.content=='function'?this.options.content.call(this.$el[0],this):this.options.content
 this.setContent(content)}}
+Popup.prototype.initEvents=function(){var self=this
+this.$container.data('oc.popup',this)
+this.$modal.on('hide.bs.modal',function(){self.triggerEvent('hide.oc.popup')
+self.isOpen=false
+self.setBackdrop(false)})
+this.$modal.on('hidden.bs.modal',function(){self.triggerEvent('hidden.oc.popup')
+self.$container.remove()
+$(document.body).removeClass('modal-open')
+self.dispose()})
+this.$modal.on('show.bs.modal',function(){self.isOpen=true
+self.setBackdrop(true)
+$(document.body).addClass('modal-open')})
+this.$modal.on('shown.bs.modal',function(){self.triggerEvent('shown.oc.popup')})
+this.$modal.on('close.oc.popup',function(){self.hide()
+return false})}
+Popup.prototype.dispose=function(){this.$modal.off('hide.bs.modal')
+this.$modal.off('hidden.bs.modal')
+this.$modal.off('show.bs.modal')
+this.$modal.off('shown.bs.modal')
+this.$modal.off('close.oc.popup')
+this.$el.off('dispose-control',this.proxy(this.dispose))
+this.$el.removeData('oc.popup')
+this.$container.removeData('oc.popup')
+this.$container=null
+this.$content=null
+this.$dialog=null
+this.$modal=null
+this.$el=null
+this.options=null
+BaseProto.dispose.call(this)}
 Popup.prototype.createPopupContainer=function(){var
 modal=$('<div />').prop({class:'control-popup modal fade',role:'dialog',tabindex:-1}),modalDialog=$('<div />').addClass('modal-dialog'),modalContent=$('<div />').addClass('modal-content')
 if(this.options.size)
@@ -3467,16 +3486,17 @@ Popup.prototype.setLoading=function(val){if(!this.$backdrop)
 return;var self=this
 if(val){setTimeout(function(){self.$backdrop.addClass('loading');},100)}
 else{this.$backdrop.removeClass('loading');}}
+Popup.prototype.setShake=function(){var self=this
+this.$content.addClass('popup-shaking')
+setTimeout(function(){self.$content.removeClass('popup-shaking')},1000)}
 Popup.prototype.hideLoading=function(val){this.setLoading(false)
 var self=this
 setTimeout(function(){self.setBackdrop(false)},250)
 setTimeout(function(){self.hide()},500)}
-Popup.prototype.triggerEvent=function(eventName,params){if(!params)
-params=[this.$el,this.$modal]
+Popup.prototype.triggerEvent=function(eventName,params){if(!params){params=[this.$el,this.$modal]}
 var eventObject=jQuery.Event(eventName,{relatedTarget:this.$container.get(0)})
 this.$el.trigger(eventObject,params)
-if(this.firstDiv)
-this.firstDiv.trigger(eventObject,params)}
+if(this.firstDiv){this.firstDiv.trigger(eventObject,params)}}
 Popup.prototype.reload=function(){this.init()}
 Popup.prototype.show=function(){this.$modal.modal('show')
 this.$modal.on('click.dismiss.popup','[data-dismiss="popup"]',$.proxy(this.hide,this))
@@ -3488,10 +3508,8 @@ this.triggerEvent('hide.oc.popup')
 if(this.allowHide)
 this.$modal.modal('hide')
 this.$dialog.css('transform','')}
-Popup.prototype.visible=function(val){if(val)
-this.$modal.addClass('in')
-else
-this.$modal.removeClass('in')
+Popup.prototype.visible=function(val){if(val){this.$modal.addClass('in')}
+else{this.$modal.removeClass('in')}
 this.setBackdrop(val)}
 Popup.prototype.toggle=function(){this.triggerEvent('popupToggle',[this.$modal])
 this.triggerEvent('toggle.oc.popup',[this.$modal])
@@ -3516,7 +3534,7 @@ catch(e){throw new Error('Error parsing the '+name+' attribute value. '+e)}}
 $(document).on('click.oc.popup','[data-control="popup"]',function(event){event.preventDefault()
 $(this).popup()});$(document).on('ajaxPromise','[data-popup-load-indicator]',function(event,context){if($(this).data('request')!=context.handler)return
 $(this).closest('.control-popup').removeClass('in').popup('setLoading',true)}).on('ajaxFail','[data-popup-load-indicator]',function(event,context){if($(this).data('request')!=context.handler)return
-$(this).closest('.control-popup').addClass('in').popup('setLoading',false)}).on('ajaxDone','[data-popup-load-indicator]',function(event,context){if($(this).data('request')!=context.handler)return
+$(this).closest('.control-popup').addClass('in').popup('setLoading',false).popup('setShake')}).on('ajaxDone','[data-popup-load-indicator]',function(event,context){if($(this).data('request')!=context.handler)return
 $(this).closest('.control-popup').popup('hideLoading')})}(window.jQuery);+function($){"use strict";var ChartUtils=function(){}
 ChartUtils.prototype.defaultValueColor='#b8b8b8';ChartUtils.prototype.getColor=function(index){var
 colors=['#95b753','#cc3300','#e5a91a','#3366ff','#ff0f00','#ff6600','#ff9e01','#fcd202','#f8ff01','#b0de09','#04d215','#0d8ecf','#0d52d1','#2a0cd0','#8a0ccf','#cd0d74','#754deb','#dddddd','#999999','#333333','#000000','#57032a','#ca9726','#990000','#4b0c25'],colorIndex=index%(colors.length-1);return colors[colorIndex];}
