749e4c031cb6290cfa1af648df7228ad7f93e879
Szabó Gergő <gergo85@gmail.com>
2016-08-27 01:14:31 +0200

    Translate the remove widget dialog (#2323)
    * Translate the remove widget dialog

--- modules/system/lang/en/client.php
+++ modules/system/lang/en/client.php
@@ -35,20 +35,21 @@ return [
     ],
 
     'mediamanager' => [
-        'insert_link' => "Insert Media Link",
-        'insert_image' => "Insert Media Image",
-        'insert_video' => "Insert Media Video",
-        'insert_audio' => "Insert Media Audio",
-        'invalid_file_empty_insert' => "Please select file to insert a links to.",
-        'invalid_file_single_insert' => "Please select a single file.",
-        'invalid_image_empty_insert' => "Please select image(s) to insert.",
-        'invalid_video_empty_insert' => "Please select a video file to insert.",
-        'invalid_audio_empty_insert' => "Please select an audio file to insert.",
+        'insert_link' => 'Insert Media Link',
+        'insert_image' => 'Insert Media Image',
+        'insert_video' => 'Insert Media Video',
+        'insert_audio' => 'Insert Media Audio',
+        'invalid_file_empty_insert' => 'Please select file to insert a links to.',
+        'invalid_file_single_insert' => 'Please select a single file.',
+        'invalid_image_empty_insert' => 'Please select image(s) to insert.',
+        'invalid_video_empty_insert' => 'Please select a video file to insert.',
+        'invalid_audio_empty_insert' => 'Please select an audio file to insert.',
     ],
 
     'alert' => [
         'confirm_button_text' => 'OK',
         'cancel_button_text' => 'Cancel',
+        'widget_remove_confirm' => 'Remove this widget?'
     ],
 
     'datepicker' => [
