0a8f169cc3976acbbd9cc0fae0c9c5763599f7df
Samuel Georges <sam@daftspunk.com>
2016-11-18 07:41:02 +1100

    Improvements to framework extras - Form validation - Button loader - Flash messages

--- modules/system/assets/js/framework.extras.js
+++ modules/system/assets/js/framework.extras.js
@@ -5,22 +5,122 @@
  * Copyright 2016 Alexey Bobkov, Samuel Georges
  * ======================================================================== */
 
-/*
- * The stripe loading indicator.
- *
- * Displays the animated loading indicator stripe at the top of the page.
- *
- * JavaScript API:
- * $.oc.stripeLoadIndicator.show(event)
- * $.oc.stripeLoadIndicator.hide()
- *
- * By default if the show() method has been called several times, the hide() method should be
- * called the same number of times in order to hide the stripe. Use hide(true) to hide the 
- * indicator forcibly.
- */
 +function ($) { "use strict";
     if ($.oc === undefined)
         $.oc = {}
+
+    // @todo Provide an interface for configuration
+    // - Custom loader CSS class
+    // - Custom stripe loader color
+
+    var LOADER_CLASS = 'oc-loading';
+
+    // FLASH HANDLING
+    // ============================
+
+    $(document).on('ajaxError', '[data-request][data-request-flash]', function(event, context, message) {
+        if (!event.isDefaultPrevented() && message) {
+            $.oc.flashMsg({ text: message, class: 'error' })
+            event.preventDefault()
+        }
+    })
+
+    $(document).on('ajaxBeforeUpdate', '[data-request][data-request-flash]', function(event, context, data) {
+        if (data['X_OCTOBER_FLASH_MESSAGES']) {
+            $.each(data['X_OCTOBER_FLASH_MESSAGES'], function(type, message) {
+                $.oc.flashMsg({ text: message, class: type })
+            })
+        }
+    })
+
+    // FORM VALIDATION
+    // ============================
+
+    $(document).on('ajaxValidation', '[data-request][data-request-validate]', function(event, context, errorMsg, fields) {
+        var $this = $(this).closest('form'),
+            $container = $('[data-validate-error]', $this),
+            messages = [],
+            $field
+
+        $.each(fields, function(fieldName, fieldMessages) {
+            $field = $('[data-validate-for='+fieldName+']', $this)
+            messages = $.merge(messages, fieldMessages)
+            if (!!$field.length) {
+                if (!$field.text().length || $field.data('emptyMode') == true) {
+                    $field
+                        .data('emptyMode', true)
+                        .text(fieldMessages.join(', '))
+                }
+                $field.addClass('visible')
+            }
+        })
+
+        if (!!$container.length) {
+            var $oldMessages = $('[data-message]', $container)
+            $container.addClass('visible')
+
+            if (!!$oldMessages.length) {
+                var $clone = $oldMessages.first()
+
+                $.each(messages, function(key, message) {
+                    $clone.clone().text(message).insertAfter($clone)
+                })
+
+                $oldMessages.remove()
+            }
+            else {
+                $container.text(errorMsg)
+            }
+        }
+
+        $this.one('ajaxError', function(event){
+            event.preventDefault()
+        })
+    })
+
+    $(document).on('ajaxPromise', '[data-request][data-request-validate]', function() {
+        var $this = $(this).closest('form')
+        $('[data-validate-for]', $this).removeClass('visible')
+        $('[data-validate-error]', $this).removeClass('visible')
+    })
+
+    // LOADING BUTTONS
+    // ============================
+
+    $(document)
+        .on('ajaxPromise', '[data-request]', function() {
+            var $target = $(this)
+
+            if ($target.data('attach-loading') !== undefined) {
+                $target
+                    .addClass(LOADER_CLASS)
+                    .prop('disabled', true)
+            }
+
+            if ($target.is('form')) {
+                $('[data-attach-loading]', $target)
+                    .addClass(LOADER_CLASS)
+                    .prop('disabled', true)
+            }
+        })
+        .on('ajaxFail ajaxDone', '[data-request]', function() {
+            var $target = $(this)
+
+            if ($target.data('attach-loading') !== undefined) {
+                $target
+                    .removeClass(LOADER_CLASS)
+                    .prop('disabled', false)
+            }
+
+            if ($target.is('form')) {
+                $('[data-attach-loading]', $target)
+                    .removeClass(LOADER_CLASS)
+                    .prop('disabled', false)
+            }
+        })
+
+    // STRIPE LOAD INDICATOR
+    // ============================
 
     var StripeLoadIndicator = function() {
         var self = this
@@ -41,8 +141,9 @@
         // Restart the animation
         this.stripe.after(this.stripe = this.stripe.clone()).remove()
 
-        if (this.counter > 1)
+        if (this.counter > 1) {
             return
+        }
 
         this.indicator.removeClass('loaded')
         $(document.body).addClass('oc-loading')
@@ -50,8 +151,10 @@
 
     StripeLoadIndicator.prototype.hide = function(force) {
         this.counter--
-        if (force !== undefined && force)
+
+        if (force !== undefined && force) {
             this.counter = 0
+        }
 
         if (this.counter <= 0) {
             this.indicator.addClass('loaded')
@@ -62,7 +165,7 @@
     $.oc.stripeLoadIndicator = new StripeLoadIndicator()
 
     // STRIPE LOAD INDICATOR DATA-API
-    // ==============
+    // ============================
 
     $(document)
         .on('ajaxPromise', '[data-request]', function(event) {
@@ -79,9 +182,75 @@
                 if ($el.closest('html').length === 0)
                     $.oc.stripeLoadIndicator.hide()
              })
-        }).on('ajaxFail ajaxDone', '[data-request]', function(event) {
+        })
+        .on('ajaxFail ajaxDone', '[data-request]', function(event) {
             event.stopPropagation()
             $.oc.stripeLoadIndicator.hide()
         })
+
+    // FLASH MESSAGE
+    // ============================
+
+    var FlashMessage = function (options, el) {
+        var
+            options = $.extend({}, FlashMessage.DEFAULTS, options),
+            $element = $(el)
+
+        $('body > p.flash-message').remove()
+
+        if ($element.length == 0) {
+            $element = $('<p />').addClass(options.class).html(options.text)
+        }
+
+        $element
+            .addClass('flash-message fade')
+            .attr('data-control', null)
+            .on('click', 'button', remove)
+            .on('click', remove)
+            .append('<button type="button" class="close" aria-hidden="true">&times;</button>')
+
+        $(document.body).append($element)
+
+        setTimeout(function() {
+            $element.addClass('in')
+        }, 100)
+
+        var timer = window.setTimeout(remove, options.interval * 1000)
+
+        function removeElement() {
+            $element.remove()
+        }
+
+        function remove() {
+            window.clearInterval(timer)
+
+            $element.removeClass('in')
+            $.support.transition && $element.hasClass('fade')
+                ? $element
+                    .one($.support.transition.end, removeElement)
+                    .emulateTransitionEnd(500)
+                : removeElement()
+        }
+    }
+
+    FlashMessage.DEFAULTS = {
+        class: 'success',
+        text: 'Default text',
+        interval: 2
+    }
+
+    if ($.oc === undefined)
+        $.oc = {}
+
+    $.oc.flashMsg = FlashMessage
+
+    // FLASH MESSAGE DATA-API
+    // ===============
+
+    $(document).render(function(){
+        $('[data-control=flash-message]').each(function(){
+            $.oc.flashMsg($(this).data(), this)
+        })
+    })
 
 }(window.jQuery);
