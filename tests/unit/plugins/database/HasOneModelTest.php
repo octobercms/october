7b0531e0a032ed4c2e44e41f524abd546fa708f5
Samuel Georges <sam@daftspunk.com>
2016-11-22 08:07:10 +1100

    Tests to prove double save issue fixed
    Has one and morph one relations would null on a second save. This is because the relation would use an UPDATE sql query to null the value, then leverage eloquent's save() method to apply it again. Eloquent's save() method does not apply attributes that are unchanged (not dirty), resulting in the value being left as null.
    
    Refs #1986

--- tests/unit/plugins/database/HasOneModelTest.php
+++ tests/unit/plugins/database/HasOneModelTest.php
@@ -30,6 +30,10 @@ class HasOneModelTest extends PluginTestCase
         $this->assertEquals($author->id, $phone1->author_id);
         $this->assertEquals('0404040404', $author->phone->number);
 
+        // Double check
+        $phone1 = Phone::find($phone1->id);
+        $this->assertEquals($author->id, $phone1->author_id);
+
         // Set by primary key
         $phoneId = $phone2->id;
         $author->phone = $phoneId;
@@ -37,6 +41,10 @@ class HasOneModelTest extends PluginTestCase
         $phone2 = Phone::find($phoneId);
         $this->assertEquals($author->id, $phone2->author_id);
         $this->assertEquals('0505050505', $author->phone->number);
+
+        // Ensure relationship is "stolen" from first model
+        $phone1 = Phone::find($phone1->id);
+        $this->assertNotEquals($author->id, $phone1->author_id);
 
         // Nullify
         $author->phone = null;
@@ -49,6 +57,25 @@ class HasOneModelTest extends PluginTestCase
         $author->phone = $phone3;
         $this->assertEquals('0606060606', $author->phone->number);
         $this->assertEquals($author->id, $phone3->author_id);
+    }
+
+    public function testSetRelationValueTwice()
+    {
+        Model::unguard();
+        $author = Author::create(['name' => 'Stevie', 'email' => 'stevie@email.tld']);
+        $phone = Phone::create(['number' => '0505050505']);
+        Model::reguard();
+
+        $phoneId = $phone->id;
+        $author->phone = $phoneId;
+        $author->save();
+
+        $author->phone = $phoneId;
+        $author->save();
+
+        $phone = Phone::find($phoneId);
+        $this->assertEquals($author->id, $phone->author_id);
+        $this->assertEquals('0505050505', $author->phone->number);
     }
 
     public function testGetRelationValue()
