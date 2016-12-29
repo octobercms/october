512c052fa6120c5b3e5616f33ddbe4b7168f176d
Michael Santoso <santosomichael94@gmail.com>
2016-12-29 09:05:28 +0700

    update life time of session on this session.php

--- config/session.php
+++ config/session.php
@@ -29,9 +29,9 @@ return [
     |
     */
 
-    'lifetime' => 120,
+    'lifetime' => 0,
 
-    'expire_on_close' => false,
+    'expire_on_close' => true,
 
     /*
     |--------------------------------------------------------------------------
