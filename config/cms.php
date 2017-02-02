dc8599358475612d2cc2022f0be6fa2d834dbb0e
Samuel Georges <sam@daftspunk.com>
2017-02-03 05:40:24 +1100

    Minor improvements Add navbar-default styling out of the box

--- config/cms.php
+++ config/cms.php
@@ -186,12 +186,12 @@ return [
     |
     | If deep hashing is enabled, the combiner cache will be reset when a change
     | is detected on imported files, in addition to those referenced directly.
-    | This will cause slower page performance. If set to null, assets are
-    | minified, when debug mode (app.debug) is disabled.
+    | This will cause slower page performance. If set to null, deep hashing
+    | is used when debug mode (app.debug) is enabled.
     |
     */
 
-    'enableAssetDeepHashing' => false,
+    'enableAssetDeepHashing' => null,
 
     /*
     |--------------------------------------------------------------------------
