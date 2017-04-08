9e74fe6a53f84b1d67cc2fa4c3f52e6a153d81ec
Christophe Vuagniaux <ChVuagniaux@users.noreply.github.com>
2017-04-08 12:21:34 +0200

    Repeaters Firefox compatibility fixes (#2738)
    * Repeaters fix Firefox collapse/expand buttons not working
    
    * Repeaters fix Firefox open new tab when CTRL + click on Expand/Collapse buttons

--- modules/backend/formwidgets/repeater/assets/js/repeater.js
+++ modules/backend/formwidgets/repeater/assets/js/repeater.js
@@ -51,7 +51,7 @@
 
     Repeater.prototype.dispose = function() {
         this.$sortable.sortable('destroy')
-        
+
         this.$el.off('ajaxDone', '> .field-repeater-items > .field-repeater-item > .repeater-item-remove > [data-repeater-remove]', this.proxy(this.onRemoveItemSuccess))
         this.$el.off('ajaxDone', '> .field-repeater-add-item > [data-repeater-add]', this.proxy(this.onAddItemSuccess))
         this.$el.off('click', '> .field-repeater-items > .field-repeater-item > .repeater-item-collapse .repeater-item-collapse-one', this.proxy(this.toggleCollapse))
@@ -102,7 +102,9 @@
         var $item = $(ev.target).closest('.field-repeater-item'),
             isCollapsed = $item.hasClass('collapsed')
 
-        if (event.ctrlKey || event.metaKey) {
+        ev.preventDefault()
+
+        if (ev.ctrlKey || ev.metaKey) {
             isCollapsed ? this.expandAll() : this.collapseAll()
         }
         else {
