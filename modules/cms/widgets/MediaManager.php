f020479b91dd2a7e3725e2dbbe87c021a8851792
Samuel Georges <sam@daftspunk.com>
2017-06-10 04:58:29 +1000

    Move session logic to SessionMaker This allows controller behaviors to access session utils

--- modules/cms/widgets/MediaManager.php
+++ modules/cms/widgets/MediaManager.php
@@ -750,12 +750,12 @@ class MediaManager extends WidgetBase
 
     protected function setSidebarVisible($visible)
     {
-        return $this->putSession('sideba_visible', !!$visible);
+        return $this->putSession('sidebar_visible', !!$visible);
     }
 
     protected function getSidebarVisible()
     {
-        return $this->getSession('sideba_visible', true);
+        return $this->getSession('sidebar_visible', true);
     }
 
     protected function itemTypeToIconClass($item, $itemType)
