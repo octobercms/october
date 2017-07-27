e9884522ea95a5000b6fc4e75c8a2bb23b0ac0b5
Samuel Georges <sam@daftspunk.com>
2017-07-27 14:01:27 +1000

    Missing forward slash

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,8 +10,8 @@ require __DIR__ . '/../bootstrap/autoload.php';
  */
 $loader = new October\Rain\Support\ClassLoader(
     new October\Rain\Filesystem\Filesystem,
-    __DIR__ . '../',
-    __DIR__ . '../storage/framework/classes.php'
+    __DIR__ . '/../',
+    __DIR__ . '/../storage/framework/classes.php'
 );
 
 $loader->register();
