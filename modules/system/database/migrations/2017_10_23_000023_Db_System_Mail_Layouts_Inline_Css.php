be511c315d4272e347ed81588fcd8aadc3aeec2f
TimFoerster <mail@tim-foerster.de>
2017-10-23 01:07:16 +0200

    Create 2017_10_23_000023_Db_System_Mail_Layouts_Inline_Css.php
    adding inline_css field

---
+++ modules/system/database/migrations/2017_10_23_000023_Db_System_Mail_Layouts_Inline_Css.php
@@ -0,0 +1,18 @@
+<?php
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+class DbSystemMailLayoutsInlineCss extends Migration
+{
+    public function up()
+    {
+        Schema::table('system_mail_layouts', function (Blueprint $table) {
+            $table->boolean('inline_css')->default(true);
+        });
+    }
+    public function down()
+    {
+        Schema::table('system_mail_layouts', function (Blueprint $table) {
+           $table->dropColumn('inlince_css');
+        });
+    }
+}
