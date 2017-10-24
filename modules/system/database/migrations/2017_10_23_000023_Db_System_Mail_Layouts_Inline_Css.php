e1f1d1117fc063be6d9449e2e355909e96b76a0c
TimFoerster <mail@tim-foerster.de>
2017-10-24 15:09:13 +0200

    formating

--- modules/system/database/migrations/2017_10_23_000023_Db_System_Mail_Layouts_Inline_Css.php
+++ modules/system/database/migrations/2017_10_23_000023_Db_System_Mail_Layouts_Inline_Css.php
@@ -1,6 +1,8 @@
 <?php
+
 use October\Rain\Database\Schema\Blueprint;
 use October\Rain\Database\Updates\Migration;
+
 class DbSystemMailLayoutsInlineCss extends Migration
 {
     public function up()
@@ -9,6 +11,7 @@ class DbSystemMailLayoutsInlineCss extends Migration
             $table->boolean('inline_css')->default(true);
         });
     }
+    
     public function down()
     {
         Schema::table('system_mail_layouts', function (Blueprint $table) {
