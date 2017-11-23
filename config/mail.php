a099e4e0862bb0756e553f6dca32b1a3402d007d
Samuel Georges <sam@daftspunk.com>
2017-11-23 11:51:33 +1100

    Change default mail driver to smtp The "mail" driver has been removed completely from the underlying libraries due to serious security concerns. Refs #3256

--- config/mail.php
+++ config/mail.php
@@ -11,11 +11,12 @@ return [
     | sending of e-mail. You may specify which one you're using throughout
     | your application here. By default, Laravel is setup for SMTP mail.
     |
-    | Supported: "smtp", "mail", "sendmail", "mailgun", "mandrill", "log"
+    | Supported: "smtp", "sendmail", "mailgun", "mandrill", "ses",
+    |            "sparkpost", "log", "array"
     |
     */
 
-    'driver' => 'mail',
+    'driver' => 'smtp',
 
     /*
     |--------------------------------------------------------------------------
