48e7640eb8595b7cd7576d807ac9cfa15e841f36
SeriousKen <ken@seriousbrewing.co.uk>
2018-06-06 09:40:01 +0100

    Fixed test

--- tests/unit/cms/classes/ContentTest.php
+++ tests/unit/cms/classes/ContentTest.php
@@ -11,7 +11,7 @@ class ContentTest extends TestCase
         $theme = Theme::load('test');
         $content = Content::load($theme, 'markdown-content.md');
 
-        $this->assertEquals('Be brave, be **bold**, live *italic*', $content->markup);
+        $this->assertEquals('Be brave, be **bold**, live *italic*', $content->content);
         $this->assertEquals('<p>Be brave, be <strong>bold</strong>, live <em>italic</em></p>', $content->parsedMarkup);
     }
 
@@ -20,7 +20,7 @@ class ContentTest extends TestCase
         $theme = Theme::load('test');
         $content = Content::load($theme, 'text-content.txt');
 
-        $this->assertEquals('Pen is <mightier> than the sword, HTML is <richer> than the text', $content->markup);
+        $this->assertEquals('Pen is <mightier> than the sword, HTML is <richer> than the text', $content->content);
         $this->assertEquals('Pen is &lt;mightier&gt; than the sword, HTML is &lt;richer&gt; than the text', $content->parsedMarkup);
     }
 
@@ -29,7 +29,7 @@ class ContentTest extends TestCase
         $theme = Theme::load('test');
         $content = Content::load($theme, 'html-content.htm');
 
-        $this->assertEquals('<a href="#">Stephen Saucier</a> changed his profile picture &mdash; <small>7 hrs ago</small></div>', $content->markup);
+        $this->assertEquals('<a href="#">Stephen Saucier</a> changed his profile picture &mdash; <small>7 hrs ago</small></div>', $content->content);
         $this->assertEquals('<a href="#">Stephen Saucier</a> changed his profile picture &mdash; <small>7 hrs ago</small></div>', $content->parsedMarkup);
     }
 
