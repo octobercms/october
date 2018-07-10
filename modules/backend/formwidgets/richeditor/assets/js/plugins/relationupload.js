dba56205a395968e21437b47f86430cbae95a748
Joakim <10329253+joakimbo@users.noreply.github.com>
2018-07-10 15:32:19 +0200

    Added buttons for relation file browsing
    prep. work, the functions will eventually call a handler from the widget

--- modules/backend/formwidgets/richeditor/assets/js/plugins/relationupload.js
+++ modules/backend/formwidgets/richeditor/assets/js/plugins/relationupload.js
@@ -16,6 +16,23 @@
                 _session_key: $('input[name="_session_key"]').val(),
             }
         }
+
+        function onInsertImage() {
+            console.log('browse upload image');
+        }
+
+        function onInsertVideo() {
+            console.log('browse upload video');
+        }
+
+        function onInsertAudio() {
+            console.log('browse upload audio');
+        }
+
+        function onInsertFile() {
+            console.log('browse upload file');
+        }
+
         /**
          * Init.
          */
@@ -25,7 +42,79 @@
 
         return {
             _init: _init,
+            insertImage: onInsertImage,
+            insertVideo: onInsertVideo,
+            insertAudio: onInsertAudio,
+            insertFile: onInsertFile,
         }
     }
+
+    $.FE.DEFAULTS.imageInsertButtons.push('relationUploadImageManager');
+
+    $.FE.RegisterCommand('relationUploadImageManager', {
+        title: 'Browse',
+        undo: false,
+        focus: false,
+        callback: function () {
+            this.relationUpload.insertImage();
+        },
+        plugin: 'relationUpload'
+    })
+
+    // Add the font size icon.
+    $.FE.DefineIcon('relationUploadImageManager', {
+        NAME: 'folder'
+    });
+
+    $.FE.DEFAULTS.videoInsertButtons.push('relationUploadVideoManager');
+
+    $.FE.RegisterCommand('relationUploadVideoManager', {
+        title: 'Browse',
+        undo: false,
+        focus: false,
+        callback: function () {
+            this.relationUpload.insertVideo();
+        },
+        plugin: 'relationUpload'
+    })
+
+    // Add the font size icon.
+    $.FE.DefineIcon('relationUploadVideoManager', {
+        NAME: 'folder'
+    });
+
+    $.FE.DEFAULTS.audioInsertButtons.push('relationUploadAudioManager');
+
+    $.FE.RegisterCommand('relationUploadAudioManager', {
+        title: 'Browse',
+        undo: false,
+        focus: false,
+        callback: function () {
+            this.relationUpload.insertAudio();
+        },
+        plugin: 'relationUpload'
+    })
+
+    // Add the font size icon.
+    $.FE.DefineIcon('relationUploadAudioManager', {
+        NAME: 'folder'
+    });
+
+    $.FE.DEFAULTS.fileInsertButtons.push('relationUploadFileManager');
+
+    $.FE.RegisterCommand('relationUploadFileManager', {
+        title: 'Browse',
+        undo: false,
+        focus: false,
+        callback: function () {
+            this.relationUpload.insertFile();
+        },
+        plugin: 'relationUpload'
+    })
+
+    // Add the font size icon.
+    $.FE.DefineIcon('relationUploadFileManager', {
+        NAME: 'folder'
+    });
 
 })(jQuery);
