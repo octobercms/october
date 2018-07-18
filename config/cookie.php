ba59e213776408a9cded40c03e4b4107c1bdd03f
Kanji Furuhashi <pikanji@gmail.com>
2018-07-18 10:54:02 +0900

    Add cookie configuration file

---
+++ config/cookie.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cookies without encryption
+    |--------------------------------------------------------------------------
+    |
+    | OctoberCMS encrypts/decrypts cookies by default. You can specify cookies
+    | that should not be encrypted or decrypted here. This is useful, for
+    | example, when you want to pass data from frontend to server side backend
+    | via cookies, and vice versa.
+    |
+    */
+
+    'unecryptedCookies' => [
+        // 'my_cookie',
+    ],
+
+];
