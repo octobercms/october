fe3af9501d2a37c426086b92bcf9c21396e7dd3c
vosco88 <jvoscek@gmail.com>
2018-08-05 10:10:32 +0200

    Update lang.php

--- modules/cms/lang/sk/lang.php
+++ modules/cms/lang/sk/lang.php
@@ -1,5 +1,4 @@
 <?php
-
 return [
     'cms_object' => [
         'invalid_file' => 'Invalid file name: :name. File names can contain only alphanumeric symbols, underscores, dashes and dots. Some examples of correct file names: page.htm, page, subdirectory/page',
@@ -7,12 +6,24 @@ return [
         'file_already_exists' => "File ':name' already exists.",
         'error_saving' => "Error saving file ':name'. Please check write permissions.",
         'error_creating_directory' => 'Error creating directory :name. Please check write permissions.',
-        'invalid_file_extension'=>'Invalid file extension: :invalid. Allowed extensions are: :allowed.',
+        'invalid_file_extension' => 'Invalid file extension: :invalid. Allowed extensions are: :allowed.',
         'error_deleting' => "Error deleting the template file ':name'. Please check write permissions.",
         'delete_success' => 'Templates deleted: :count.',
-        'file_name_required' => 'The File Name field is required.'
+        'file_name_required' => 'The File Name field is required.',
+        'safe_mode_enabled' => 'Safe mode is currently enabled.'
+    ],
+    'dashboard' => [
+        'active_theme' => [
+            'widget_title_default' => 'Website',
+            'online' => 'Online',
+            'maintenance' => 'In maintenance',
+            'manage_themes' => 'Manage themes',
+            'customize_theme' => 'Customize theme'
+        ]
     ],
     'theme' => [
+        'not_found_name' => "The theme ':name' is not found.",
+        'by_author' => 'By :name',
         'active' => [
             'not_set' => 'The active theme is not set.',
             'not_found' => 'The active theme is not found.'
@@ -23,19 +34,74 @@ return [
             'not_match' => "The object you're trying to access doesn't belong to the theme being edited. Please reload the page."
         ],
         'settings_menu' => 'Front-end theme',
-        'settings_menu_description' => 'Preview the list of installed themes and select an active theme.',
-        'find_more_themes' => 'Find more themes on OctoberCMS Theme Marketplace.',
+        'settings_menu_description' => 'Manage the front-end theme and customization options.',
+        'default_tab' => 'Properties',
+        'name_label' => 'Name',
+        'name_create_placeholder' => 'New theme name',
+        'author_label' => 'Author',
+        'author_placeholder' => 'Person or company name',
+        'description_label' => 'Description',
+        'description_placeholder' => 'Theme description',
+        'homepage_label' => 'Homepage',
+        'homepage_placeholder' => 'Website URL',
+        'code_label' => 'Code',
+        'code_placeholder' => 'A unique code for this theme used for distribution',
+        'preview_image_label' => 'Preview image',
+        'preview_image_placeholder' => 'The path of theme preview image.',
+        'dir_name_label' => 'Directory name',
+        'dir_name_create_label' => 'The destination theme directory',
+        'theme_label' => 'Theme',
+        'theme_title' => 'Themes',
         'activate_button' => 'Activate',
         'active_button' => 'Activate',
-        'customize_button' => 'Customize'
+        'customize_theme' => 'Customize Theme',
+        'customize_button' => 'Customize',
+        'duplicate_button' => 'Duplicate',
+        'duplicate_title' => 'Duplicate theme',
+        'duplicate_theme_success' => 'Theme duplicated!',
+        'manage_button' => 'Manage',
+        'manage_title' => 'Manage theme',
+        'edit_properties_title' => 'Theme',
+        'edit_properties_button' => 'Edit properties',
+        'save_properties' => 'Save properties',
+        'import_button' => 'Import',
+        'import_title' => 'Import theme',
+        'import_theme_success' => 'Theme imported!',
+        'import_uploaded_file' => 'Theme archive file',
+        'import_overwrite_label' => 'Overwrite existing files',
+        'import_overwrite_comment' => 'Untick this box to only import new files',
+        'import_folders_label' => 'Folders',
+        'import_folders_comment' => 'Please select the theme folders you would like to import',
+        'export_button' => 'Export',
+        'export_title' => 'Export theme',
+        'export_folders_label' => 'Folders',
+        'export_folders_comment' => 'Please select the theme folders you would like to export',
+        'delete_button' => 'Delete',
+        'delete_confirm' => 'Delete this theme? It cannot be undone!',
+        'delete_active_theme_failed' => 'Cannot delete the active theme, try making another theme active first.',
+        'delete_theme_success' => 'Theme deleted!',
+        'create_title' => 'Create theme',
+        'create_button' => 'Create',
+        'create_new_blank_theme' => 'Create a new blank theme',
+        'create_theme_success' => 'Theme created!',
+        'create_theme_required_name' => 'Please specify a name for the theme.',
+        'new_directory_name_label' => 'Theme directory',
+        'new_directory_name_comment' => 'Provide a new directory name for the duplicated theme.',
+        'dir_name_invalid' => 'Name can contain only digits, Latin letters and the following symbols: _-',
+        'dir_name_taken' => 'Desired theme directory already exists.',
+        'find_more_themes' => 'Find more themes',
+        'saving' => 'Saving theme...',
+        'return' => 'Return to themes list'
     ],
     'maintenance' => [
         'settings_menu' => 'Maintenance mode',
         'settings_menu_description' => 'Configure the maintenance mode page and toggle the setting.',
         'is_enabled' => 'Enable maintenance mode',
-        'is_enabled_comment' => 'When activated website visitors will see the page chosen below.'
+        'is_enabled_comment' => 'Select the page to show when maintenance mode is activated.',
+        'hint' => 'Maintenance mode will display the maintenance page to visitors who are not signed in to the back-end area.'
     ],
     'page' => [
+        'not_found_name' => "The page ':name' is not found",
         'not_found' => [
             'label' => 'Page not found',
             'help' => 'The requested page cannot be found.'
@@ -49,9 +115,13 @@ return [
         'no_list_records' => 'No pages found',
         'new' => 'New page',
         'invalid_url' => 'Invalid URL format. The URL should start with the forward slash symbol and can contain digits, Latin letters and the following symbols: ._-[]:?|/+*^$',
-        'delete_confirm_multiple' => 'Do you really want to delete selected pages?',
-        'delete_confirm_single' => 'Do you really want delete this page?',
-        'no_layout' => '-- no layout --'
+        'delete_confirm_multiple' => 'Delete selected pages?',
+        'delete_confirm_single' => 'Delete this page?',
+        'no_layout' => '-- no layout --',
+        'cms_page' => 'CMS page',
+        'title' => 'Page title',
+        'url' => 'Page URL',
+        'file_name' => 'Page file name'
     ],
     'layout' => [
         'not_found_name' => "The layout ':name' is not found",
@@ -59,8 +129,8 @@ return [
         'unsaved_label' => 'Unsaved layout(s)',
         'no_list_records' => 'No layouts found',
         'new' => 'New layout',
-        'delete_confirm_multiple' => 'Do you really want to delete selected layouts?',
-        'delete_confirm_single' => 'Do you really want delete this layout?'
+        'delete_confirm_multiple' => 'Delete selected layouts?',
+        'delete_confirm_single' => 'Delete this layout?'
     ],
     'partial' => [
         'not_found_name' => "The partial ':name' is not found.",
@@ -68,8 +138,8 @@ return [
         'menu_label' => 'Partials',
         'unsaved_label' => 'Unsaved partial(s)',
         'no_list_records' => 'No partials found',
-        'delete_confirm_multiple' => 'Do you really want to delete selected partials?',
-        'delete_confirm_single' => 'Do you really want delete this partial?',
+        'delete_confirm_multiple' => 'Delete selected partials?',
+        'delete_confirm_single' => 'Delete this partial?',
         'new' => 'New partial'
     ],
     'content' => [
@@ -77,8 +147,8 @@ return [
         'menu_label' => 'Content',
         'unsaved_label' => 'Unsaved content',
         'no_list_records' => 'No content files found',
-        'delete_confirm_multiple' => 'Do you really want to delete selected content files or directories?',
-        'delete_confirm_single' => 'Do you really want delete this content file?',
+        'delete_confirm_multiple' => 'Delete selected content files or directories?',
+        'delete_confirm_single' => 'Delete this content file?',
         'new' => 'New content file'
     ],
     'ajax_handler' => [
@@ -110,7 +180,11 @@ return [
         'hidden' => 'Hidden',
         'hidden_comment' => 'Hidden pages are accessible only by logged-in back-end users.',
         'enter_fullscreen' => 'Enter fullscreen mode',
-        'exit_fullscreen' => 'Exit fullscreen mode'
+        'exit_fullscreen' => 'Exit fullscreen mode',
+        'open_searchbox' => 'Open Search box',
+        'close_searchbox'  => 'Close Search box',
+        'open_replacebox' => 'Open Replace box',
+        'close_replacebox'  => 'Close Replace box'
     ],
     'asset' => [
         'menu_label' => 'Assets',
@@ -132,7 +206,7 @@ return [
         'invalid_path' => 'Path can contain only digits, Latin letters, spaces and the following symbols: ._-/',
         'error_deleting_file' => 'Error deleting file :name.',
         'error_deleting_dir_not_empty' => 'Error deleting directory :name. The directory is not empty.',
-        'error_deleting_dir' => 'Error deleting file :name.',
+        'error_deleting_dir' => 'Error deleting directory :name.',
         'invalid_name' => 'Name can contain only digits, Latin letters, spaces and the following symbols: ._-',
         'original_not_found' => 'Original file or directory not found',
         'already_exists' => 'File or directory with this name already exists',
@@ -152,6 +226,8 @@ return [
         'error_moving_file' => 'Error moving file :file',
         'error_moving_directory' => 'Error moving directory :dir',
         'error_deleting_directory' => 'Error deleting the original directory :dir',
+        'no_list_records' => 'No files found',
+        'delete_confirm' => 'Delete selected files or directories?',
         'path' => 'Path'
     ],
     'component' => [
@@ -168,16 +244,49 @@ return [
     ],
     'template' => [
         'invalid_type' => 'Unknown template type.',
-        'not_found' => 'Requested template not found.',
-        'saved'=> 'Template saved.'
+        'not_found' => 'Template not found.',
+        'saved' => 'Template saved.',
+        'no_list_records' => 'No records found',
+        'delete_confirm' => 'Delete selected templates?',
+        'order_by' => 'Order by'
     ],
     'permissions' => [
-        'name' => 'Cms',
-        'manage_content' => 'Manage content',
-        'manage_assets' => 'Manage assets',
-        'manage_pages' => 'Manage pages',
-        'manage_layouts' => 'Manage layouts',
-        'manage_partials' => 'Manage partials',
-        'manage_themes' => 'Manage themes'
-    ]
+        'name' => 'CMS',
+        'manage_content' => 'Manage website content files',
+        'manage_assets' => 'Manage website assets - images, JavaScript files, CSS files',
+        'manage_pages' => 'Create, modify and delete website pages',
+        'manage_layouts' => 'Create, modify and delete CMS layouts',
+        'manage_partials' => 'Create, modify and delete CMS partials',
+        'manage_themes' => 'Activate, deactivate and configure CMS themes',
+        'manage_theme_options' => 'Configure customization options for the active theme',
+    ],
+    'theme_log' => [
+        'hint' => 'This log displays any changes made to the theme by administrators in the back-end area.',
+        'menu_label' => 'Theme log',
+        'menu_description' => 'View changes made to the active theme.',
+        'empty_link' => 'Empty theme log',
+        'empty_loading' => 'Emptying theme log...',
+        'empty_success' => 'Theme log emptied',
+        'return_link' => 'Return to theme log',
+        'id' => 'ID',
+        'id_label' => 'Log ID',
+        'created_at' => 'Date & Time',
+        'user' => 'User',
+        'type' => 'Type',
+        'type_create' => 'Create',
+        'type_update' => 'Update',
+        'type_delete' => 'Delete',
+        'theme_name' => 'Theme',
+        'theme_code' => 'Theme code',
+        'old_template' => 'Template (Old)',
+        'new_template' => 'Template (New)',
+        'template' => 'Template',
+        'diff' => 'Changes',
+        'old_value' => 'Old value',
+        'new_value' => 'New value',
+        'preview_title' => 'Template changes',
+        'template_updated' => 'Template was updated',
+        'template_created' => 'Template was created',
+        'template_deleted' => 'Template was deleted',
+    ],
 ];
