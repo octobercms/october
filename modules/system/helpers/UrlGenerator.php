bc62208a8abfc9ee8b5b48758cef0e8e998af982
Tschallacka <mdibbets@outlook.com>
2018-09-20 01:06:55 +0200

    Added a modification for windows users, to catch those forward slashes

--- modules/system/helpers/UrlGenerator.php
+++ modules/system/helpers/UrlGenerator.php
@@ -8,8 +8,15 @@ class UrlGenerator extends BaseUrlGenerator
     {
         $url = parent::to($path, $extra, $secure);
         
+        // for systems that don't use / for a directorry seperator
+        // and coders that re-use a part of a path that has a windows
+        // directory for example(results of glob, etc..)
+        if(DIRECTORY_SEPARATOR !== '/') {       
+            $url = str_replace(DIRECTORY_SEPARATOR, '/', $path);
+        }
+        
         $url = preg_replace('/([^:])(\/{2,})/', '$1/', $url);
-
+        
         return $url;
     }
 }
