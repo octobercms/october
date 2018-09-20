1008ca8199e159c15e697f3bca749ce25890a5ba
tschallacka <mdibbets@yahoo.com>
2018-09-20 08:42:35 +0200

    Updated for changes made to helpers pull request

--- tests/unit/support/HelperTest.php
+++ tests/unit/support/HelperTest.php
@@ -10,7 +10,7 @@ class HelperTest extends TestCase
         $base = url(implode('/', [config('app.url'), config('cms.themesPath')]));
         $this->assertEquals($base, $generated);
         
-        $base .= '/test/assets/js/script1.js';
+        $base = url($base . '/test/assets/js/script1.js');
         $this->assertEquals($base, themes_url('test/assets/js/script1.js'));
         $this->assertEquals($base, themes_url('/test/assets/js/script1.js'));
     }
@@ -21,7 +21,7 @@ class HelperTest extends TestCase
         $base = url(implode('/', [config('app.url'), config('cms.themesPath'), config('cms.activeTheme')]));
         $this->assertEquals($base, $generated);
         
-        $base .= '/assets/js/script1.js';
+        $base = url($base . '/assets/js/script1.js');
         $this->assertEquals($base, theme_url('/assets/js/script1.js'));
         $this->assertEquals($base, theme_url('assets/js/script1.js'));
         
@@ -29,71 +29,19 @@ class HelperTest extends TestCase
     
     public function testPluginUrl()
     {
-        $base = url(implode('/', [config('app.url'), config('cms.pluginsPath'), 'tests/unit']));
+        $base = url(implode('/', [config('app.url'), config('cms.pluginsPath')]));
         
         
-        $url =  plugin_url('', $this);
+        $url =  plugins_url(''); 
         $this->assertEquals($url, $base);
         
-        $url =  plugin_url('', 'Tests\\Unit\\Support\\HelperTest');
+        $url =  plugins_url();
         $this->assertEquals($url, $base);
         
-        $url =  plugin_url('', '\\Tests\\Unit\\Support\\HelperTest');
+        $testfile = '/author/plugin/assets/scripts/script.js';
+        $base = url($base . $testfile);
+        
+        $url =  plugins_url($testfile);
         $this->assertEquals($url, $base);
-        
-        $url =  plugin_url();
-        $this->assertEquals($url, $base);
-        
-        $testfile = '/assets/scripts/script.js';
-        $base .= $testfile;
-        
-        $url =  plugin_url($testfile, $this);
-        $this->assertEquals($url, $base);
-        
-        $url =  plugin_url($testfile, 'Tests\\Unit\\Support\\HelperTest');
-        $this->assertEquals($url, $base);
-        
-        $url =  plugin_url($testfile, '\\Tests\\Unit\\Support\\HelperTest');
-        $this->assertEquals($url, $base);
-        
-        $url =  plugin_url($testfile);
-        $this->assertEquals($url, $base);
-        
-        $testfile = 'assets/scripts/script.js';
-        
-        $url =  plugin_url($testfile, $this);
-        $this->assertEquals($url, $base);
-        
-        $url =  plugin_url($testfile, 'Tests\\Unit\\Support\\HelperTest');
-        $this->assertEquals($url, $base);
-        
-        $url =  plugin_url($testfile, '\\Tests\\Unit\\Support\\HelperTest');
-        $this->assertEquals($url, $base);
-        
-        $url =  plugin_url($testfile);
-        $this->assertEquals($url, $base);
-    }
-    
-    public function testPluginUrlWithInvalidClassArgument()
-    {
-        $this->expectException(\InvalidArgumentException::class);
-        
-        plugin_url('', new \stdClass());
-    }
-    
-    public function testPluginUrlWithInvalidCallHistory()
-    {
-        $this->expectException(\InvalidArgumentException::class);
-        
-        testPluginUrlWithInvalidCallHistory();
     }
 }
-
-/**
- * This erases the "object" from position 2 when plugin_url is called
- * and should cause an InvalidArgumentException.
- */
-function testPluginUrlWithInvalidCallHistory()
-{
-    plugin_url();
-};
