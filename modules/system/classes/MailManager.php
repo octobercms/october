edcc4af68aa70dc4b84b687e34edb90d14191ca0
Levente Huszko <levente.huszko@gmail.com>
2018-09-25 00:54:16 +0200

    Properly utilize the plainOnly flag in mail.beforeAddContent (#3479)
    Credit to @hlev

--- modules/system/classes/MailManager.php
+++ modules/system/classes/MailManager.php
@@ -56,6 +56,7 @@ class MailManager
 
     /**
      * Same as `addContentToMailer` except with raw content.
+     *
      * @return bool
      */
     public function addRawContentToMailer($message, $content, $data)
@@ -70,11 +71,16 @@ class MailManager
     }
 
     /**
-     * This function hijacks the `addContent` method of the `October\Rain\Mail\Mailer` 
+     * This function hijacks the `addContent` method of the `October\Rain\Mail\Mailer`
      * class, using the `mailer.beforeAddContent` event.
+     *
+     * @param \Illuminate\Mail\Message $message
+     * @param string $code
+     * @param array $data
+     * @param bool $plainOnly Add only plain text content to the message
      * @return bool
      */
-    public function addContentToMailer($message, $code, $data)
+    public function addContentToMailer($message, $code, $data, $plainOnly = false)
     {
         if (isset($this->templateCache[$code])) {
             $template = $this->templateCache[$code];
@@ -87,16 +93,21 @@ class MailManager
             return false;
         }
 
-        $this->addContentToMailerInternal($message, $template, $data);
+        $this->addContentToMailerInternal($message, $template, $data, $plainOnly);
 
         return true;
     }
 
     /**
      * Internal method used to share logic between `addRawContentToMailer` and `addContentToMailer`
+     *
+     * @param \Illuminate\Mail\Message $message
+     * @param string $template
+     * @param array $data
+     * @param bool $plainOnly Add only plain text content to the message
      * @return void
      */
-    protected function addContentToMailerInternal($message, $template, $data)
+    protected function addContentToMailerInternal($message, $template, $data, $plainOnly = false)
     {
         /*
          * Start twig transaction
@@ -124,12 +135,14 @@ class MailManager
             'subject' => $swiftMessage->getSubject()
         ];
 
-        /*
-         * HTML contents
-         */
-        $html = $this->renderTemplate($template, $data);
+        if (!$plainOnly) {
+            /*
+             * HTML contents
+             */
+            $html = $this->renderTemplate($template, $data);
 
-        $message->setBody($html, 'text/html');
+            $message->setBody($html, 'text/html');
+        }
 
         /*
          * Text contents
@@ -200,9 +213,8 @@ class MailManager
 
     /**
      * Render the Markdown template into text.
-     *
-     * @param  string  $view
-     * @param  array  $data
+     * @param $content
+     * @param array $data
      * @return string
      */
     public function renderText($content, $data = [])
