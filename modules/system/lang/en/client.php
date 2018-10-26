00122108b1e53f12db2738fd7b7e7d8be64ca3d1
Alwin Drenth <adrenth@gmail.com>
2018-10-26 17:14:56 +0200

    Update Dutch (NL) translation (#3895)
    Credit to @adrenth

--- modules/system/lang/en/client.php
+++ modules/system/lang/en/client.php
@@ -78,7 +78,6 @@ return [
             'min_placeholder' => 'Min',
             'max_placeholder' => 'Max'
         ]
-
     ],
     'eventlog' => [
         'show_stacktrace' => 'Show the stacktrace',
