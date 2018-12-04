2002fd6b4b2e9241572501307c81d5f2ef2e1eea
Ben Thomson <bennothommo@users.noreply.github.com>
2018-12-05 01:22:07 +0800

    Prevent plugins that cannot be instantiated from being loaded (#3956)
    Credit to @bennothommo

---
+++ tests/fixtures/plugins/testvendor/goto/Plugin.php
@@ -0,0 +1,16 @@
+<?php namespace TestVendor\Goto;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'Invalid Test Plugin',
+            'description' => 'Test plugin used by unit tests to detect plugins with invalid namespaces.',
+            'author' => 'Test Vendor'
+        ];
+    }
+}
