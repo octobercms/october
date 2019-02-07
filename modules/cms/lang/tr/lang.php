c2ad91cac928bda41fb5ad71b5d02ecf618c769a
Mahmut Namli <mahmudnamli@gmail.com>
2019-02-07 16:15:34 +0300

    Update lang.php
    New strings from en/lang.php added and also translated into Turkish language

--- modules/cms/lang/tr/lang.php
+++ modules/cms/lang/tr/lang.php
@@ -19,18 +19,20 @@ return [
             'online' => 'yayında',
             'maintenance' => 'bakım modunda',
             'manage_themes' => 'Temaları yönet',
-        ]
+            'customize_theme' => 'Tema özelleştir',
+        ],
     ],
     'theme' => [
         'not_found_name' => "':name' isimli tema bulunamadı.",
+        'by_author' => ':name ile filtrele',
         'active' => [
             'not_set' => "Aktif tema belirtilmedi.",
-            'not_found' => 'Aktif tema bulunamadı.'
+            'not_found' => 'Aktif tema bulunamadı.',
         ],
         'edit' => [
             'not_set' => "Düzenlenecek tema belirtilmedi.",
             'not_found' => "Düzenlenecek tema bulunamadı.",
-            'not_match' => "Ulaşmaya çalıştığınız nesne düzenlenecek temaya ait değil. Lütfen sayfayı yenileyin."
+            'not_match' => "Ulaşmaya çalıştığınız nesne düzenlenecek temaya ait değil. Lütfen sayfayı yenileyin.",
         ],
         'settings_menu' => 'Temalar',
         'settings_menu_description' => 'Yüklü temalar listesini önizleyebilir, bir tema seçip aktifleştirebilirsiniz.',
@@ -45,6 +47,8 @@ return [
         'homepage_placeholder' => 'Anasayfa Adresi(URL)',
         'code_label' => 'Kod',
         'code_placeholder' => 'Temanın dağıtımında kullanılmak üzere benzersiz bir kod.',
+        'preview_image_label' => 'Önizleme görseli',
+        'preview_image_placeholder' => 'Tema önizleme görselinin yolu.',
         'dir_name_label' => 'Klasör ismi',
         'dir_name_create_label' => 'Hedef tema dizini',
         'theme_label' => 'Tema',
@@ -88,7 +92,7 @@ return [
         'dir_name_taken' => 'İstenen tema dizini zaten mevcut.',
         'find_more_themes' => 'Daha fazla tema bulun',
         'saving' => 'Tema kaydediliyor...',
-        'return' => 'Tema listesine geri dön'
+        'return' => 'Tema listesine geri dön',
     ],
     'maintenance' => [
         'settings_menu' => 'Bakım modu',
@@ -114,7 +118,11 @@ return [
         'invalid_url' => 'Hatalı URL formatı. URL eğik çizgi ile başlamalı ve sayı, Latin harfleri ve aşağıdaki sembolleri içerebilir: ._-[]:?|/+*^$',
         'delete_confirm_multiple' => 'Seçili sayfaları silmek istediğinize emin misiniz?',
         'delete_confirm_single' => 'Bu sayfayı silmek istediğinize emin misiniz?',
-        'no_layout' => '-- şablon yok --'
+        'no_layout' => '-- şablon yok --',
+        'cms_page' => 'CMS sayfa',
+        'title' => 'Sayfa başlığı',
+        'url' => 'Sayfa URL',
+        'file_name' => 'Sayfa dosyası adı',
     ],
     'layout' => [
         'not_found_name' => "':name' isimli şablon bulunamadı",
@@ -123,7 +131,7 @@ return [
         'no_list_records' => 'Şablon bulunamadı',
         'new' => 'Şablon oluştur',
         'delete_confirm_multiple' => 'Seçili şablonları silmek istediğinize emin misiniz?',
-        'delete_confirm_single' => 'Seçili şablonu silmek istediğinize emin misiniz?'
+        'delete_confirm_single' => 'Seçili şablonu silmek istediğinize emin misiniz?',
     ],
     'partial' => [
         'not_found_name' => "':name' bölümü bulunamadı.",
@@ -133,7 +141,7 @@ return [
         'no_list_records' => 'Bölüm bulunamadı.',
         'delete_confirm_multiple' => 'Seçili bölümleri silmek istediğinize emin misiniz?',
         'delete_confirm_single' => 'Bu bölümü silmek istediğinize emin misiniz?',
-        'new' => 'Bölüm Oluştur'
+        'new' => 'Bölüm Oluştur',
     ],
     'content' => [
         'not_found_name' => "':name' isminde içerik dosyası bulunamadı.",
@@ -142,18 +150,18 @@ return [
         'no_list_records' => 'İçerik dosyası bulunamadı.',
         'delete_confirm_multiple' => 'Seçili içerik dosyaları veya klasörlerini silmek istediğinize emin misiniz?',
         'delete_confirm_single' => 'Bu içerik dosyasını silmek istediğinize emin misiniz?',
-        'new' => 'Yeni İçerik'
+        'new' => 'Yeni İçerik',
     ],
     'ajax_handler' => [
         'invalid_name' => "Hatalı AJAX işleyici adı: :name.",
         'not_found' => "':name' isimli AJAX işleyici bulunamadı.",
     ],
     'cms' => [
-        'menu_label' => "Tasarım Ayarları"
+        'menu_label' => "Tasarım Ayarları",
     ],
     'sidebar' => [
         'add' => 'Ekle',
-        'search' => 'Ara...'
+        'search' => 'Ara...',
     ],
     'editor' => [
         'settings' => 'Ayarlar',
@@ -177,7 +185,7 @@ return [
         'open_searchbox' => 'Arama kutusunu aç',
         'close_searchbox'  => 'Arama kutusunu kapat',
         'open_replacebox' => 'Düzenleme kutusunu aç',
-        'close_replacebox'  => 'Düzenleme kutusunu kapat'
+        'close_replacebox'  => 'Düzenleme kutusunu kapat',
     ],
     'asset' => [
         'menu_label' => "Dosyalar",
@@ -219,7 +227,9 @@ return [
         'error_moving_file' => ':file dosyası taşınırken hatayla karşılaşıldı',
         'error_moving_directory' => ':dir klasörü taşınırken hatayla karşılaşıldı',
         'error_deleting_directory' => ':dir klasörü silinirken hatayla karşılaşıldı',
-        'path' => 'Yol'
+        'no_list_records' => 'Dosya bulunamadı',
+        'delete_confirm' => 'Seçilen dosyalar veya dizinler silinsin mi?',
+        'path' => 'Yol',
     ],
     'component' => [
         'menu_label' => "Bileşenler",
@@ -236,7 +246,10 @@ return [
     'template' => [
         'invalid_type' => "Hatalı şablon tipi.",
         'not_found' => "İstenilen şablon bulunamadı.",
-        'saved'=> "Şablon başarıyla kaydedildi."
+        'saved'=> "Şablon başarıyla kaydedildi.",
+        'no_list_records' => 'Kayıt bulunamadı',
+        'delete_confirm' => 'Seçilen şablonlar silinsin mi?',
+        'order_by' => 'Sırala',
     ],
     'permissions' => [
         'name' => 'CMS Sistemi',
@@ -246,5 +259,35 @@ return [
         'manage_layouts' => 'Şablonları düzenleyebilsin',
         'manage_partials' => 'Parça Kodları düzenleyebilsin',
         'manage_themes' => 'Temaları düzenleyebilsin',
+        'manage_theme_options' => 'Aktif tema için özelleştirme seçeneklerini yapılandırın',
+    ],
+    'theme_log' => [
+        'hint' => 'Bu kayıtlar, backend yöneticileri tarafından temada yapılan değişiklikleri görüntüler.',
+        'menu_label' => 'Tema log',
+        'menu_description' => 'Aktif temada yapılan değişiklikleri görüntüleyin.',
+        'empty_link' => 'Tema loglarını temizle',
+        'empty_loading' => 'Tema logları temizleniyor...',
+        'empty_success' => 'Tema log temizlendi',
+        'return_link' => 'Tema loglarına dön',
+        'id' => 'ID',
+        'id_label' => 'Log ID',
+        'created_at' => 'Tarih ve Saat',
+        'user' => 'Kişi',
+        'type' => 'Tipi',
+        'type_create' => 'Oluştur',
+        'type_update' => 'Güncelle',
+        'type_delete' => 'Sil',
+        'theme_name' => 'Tema',
+        'theme_code' => 'Tema kodu',
+        'old_template' => 'Şablon (Eski)',
+        'new_template' => 'Şablon (Yeni)',
+        'template' => 'Şablon',
+        'diff' => 'Değişiklikler',
+        'old_value' => 'Eski değer',
+        'new_value' => 'Yeni değer',
+        'preview_title' => 'Şablon değişiklikleri',
+        'template_updated' => 'Şablon güncellendi',
+        'template_created' => 'Şablon oluşturuldu',
+        'template_deleted' => 'Şablon silindi',
     ],
 ];
