9ddc4d627cfbe533687253550f60e90ce7ad5f68
Lucas Zamora <lucaszdv@gmail.com>
2019-02-08 21:36:31 -0300

    I added to the CLI this commands:  plugin:list, plugin:enable & plugin:disable.

---
+++ modules/system/console/PluginList.php
@@ -0,0 +1,67 @@
+<?php namespace System\Console;
+
+use Illuminate\Console\Command;
+use System\Models\PluginVersion;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
+
+/**
+ * Console command to list existing plugins.
+ *
+ * @package october\system
+ * @author Lucas Zamora
+ */
+class PluginList extends Command
+{
+    /**
+     * The console command name.
+     * @var string
+     */
+    protected $name = 'plugin:list';
+
+    /**
+     * The console command description.
+     * @var string
+     */
+    protected $description = 'List existing plugins.';
+
+    /**
+     * Execute the console command.
+     * @return void
+     */
+    public function handle()
+    {
+        $allPlugins  = PluginVersion::all();
+
+        if (count($allPlugins) <= 0 ) {
+
+            $this->info('No plugin found');
+
+            return;
+        }
+
+        // Create a new Table instance.
+        $table = new Table($this->output);
+
+        // Set the table headers.
+        $table->setHeaders([
+            'Plugin name', 'Version','Updates enabled', 'Plugin enabled'
+        ]);
+
+        // Create a new TableSeparator instance.
+        $separator = new TableSeparator;
+
+        $pluginTable = [];
+
+        foreach ($allPlugins as $plugin) {
+            $pluginTable[] = [$plugin->code, $plugin->version, (!$plugin->is_frozen) ? 'Yes': 'No', (!$plugin->is_disabled) ? 'Yes': 'No'];
+            $pluginTable[] = $separator;
+        }
+
+        // Set the contents of the table.
+        $table->setRows($pluginTable);
+
+        // Render the table to the output.
+        $table->render();
+    }
+}
