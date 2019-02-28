df4c6980c0d834c88dde7c886651aa5d9dd93d9a
Ayumi Hamasaki <asianspawebsite@gmail.com>
2019-02-28 00:35:11 +0000

    Wrapper to run Code only in HTTPs Connections
    As per discussion.

--- modules/backend/assets/js/auth/uninstall-sw.js
+++ modules/backend/assets/js/auth/uninstall-sw.js
@@ -1,7 +1,11 @@
-// Unregister all service workers before signing in to prevent cache issues
-navigator.serviceWorker.getRegistrations().then(
-    function(registrations) {
-        for (let registration of registrations) {  
-            registration.unregister();
-        }
-});
+// Wrapper to run the code only for HTTPs Connection
+if (location.protocol != 'https:')
+{
+    // Unregister all service workers before signing in to prevent cache issues
+    navigator.serviceWorker.getRegistrations().then(
+        function(registrations) {
+            for (let registration of registrations) {  
+                registration.unregister();
+            }
+    });
+}
