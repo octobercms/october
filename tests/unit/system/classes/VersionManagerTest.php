2e0d2d38885bb2b447f7b9a5aeff2ff801e70f74
Siarhei Karavai <sergey.karavay@gmail.com>
2019-03-31 17:46:33 +0300

    Allow subdirectories in migration file names (#4225)
    Credit to @GinoPane. Fixes #4224.

--- tests/unit/system/classes/VersionManagerTest.php
+++ tests/unit/system/classes/VersionManagerTest.php
@@ -186,6 +186,22 @@ class VersionManagerTest extends TestCase
                     'file.name.with.dots.php',
                     '1.0.2.scripts_can_go_anywhere.php'
                 ]
+            ],
+            [
+                [
+                    'subdirectory/file.name.with.dots.php',
+                    'The first update comment',
+                    'subdirectory\1.0.2.scripts_can_go_anywhere.php',
+                    'The second update comment',
+                ],
+                [
+                    'The first update comment',
+                    'The second update comment'
+                ],
+                [
+                    'subdirectory/file.name.with.dots.php',
+                    'subdirectory\1.0.2.scripts_can_go_anywhere.php'
+                ]
             ]
         ];
     }
