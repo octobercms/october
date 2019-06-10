311b39c0f48e1119fb27b59546ad2106a489afd9
panakour <panakourweb@gmail.com>
2019-06-10 12:01:51 +0300

    fixed test hard coded theme url

--- tests/unit/cms/classes/ControllerTest.php
+++ tests/unit/cms/classes/ControllerTest.php
@@ -27,10 +27,10 @@ class ControllerTest extends TestCase
         $controller = new Controller($theme);
 
         $url = $controller->themeUrl();
-        $this->assertEquals('http://localhost/themes/test', $url);
+        $this->assertEquals(url('/themes/test'), $url);
 
         $url = $controller->themeUrl('foo/bar.css');
-        $this->assertEquals('http://localhost/themes/test/foo/bar.css', $url);
+        $this->assertEquals(url('/themes/test').'/foo/bar.css', $url);
 
         //
         // These tests seem to bear different results
