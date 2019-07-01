1f37039190c8e70cb5ee03fff874b3964e160bf6
Ben Thomson <git@alfreido.com>
2019-07-01 23:02:17 +0800

    Add additional test cases for PluginManager
    - Add dependency tests
    - Add tests that check wrong casing in requirements
    - Clean up current tests
    - Provides test case for https://github.com/octobercms/october/pull/4337

---
+++ tests/fixtures/plugins/dependencytest/found/Plugin.php
@@ -0,0 +1,20 @@
+<?php namespace DependencyTest\Found;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public $require = [
+        'DependencyTest.Dependency'
+    ];
+
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'Dependency Test - Found',
+            'description' => 'This is a test plugin with a dependency that exists.',
+            'author' => 'Ben Thomson'
+        ];
+    }
+
+}
