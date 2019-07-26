02ba765a9ea960631ac06b106683ec699de0800e
Anurat Chapanond <anurat@gmail.com>
2019-07-27 03:52:52 +0700

    Added Thai translation (#4472)
    Credit to @anurat.

--- modules/backend/models/Preference.php
+++ modules/backend/models/Preference.php
@@ -213,6 +213,7 @@ class Preference extends Model
             'ru'    => [Lang::get('system::lang.locale.ru'),    'flag-ru'],
             'sk'    => [Lang::get('system::lang.locale.sk'),    'flag-sk'],
             'sv'    => [Lang::get('system::lang.locale.sv'),    'flag-se'],
+            'th'    => [Lang::get('system::lang.locale.th'),    'flag-th'],
             'tr'    => [Lang::get('system::lang.locale.tr'),    'flag-tr'],
             'uk'    => [Lang::get('system::lang.locale.uk'),    'flag-ua'],
             'vn'    => [Lang::get('system::lang.locale.vn'),    'flag-vn'],
