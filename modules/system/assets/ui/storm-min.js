863a71bc2d90bc5e0413aec60ac15b4abeb92443
Dan Harrin <git@DanHarrin.com>
2019-08-11 22:03:20 +0100

    Update from root

--- modules/system/assets/ui/storm-min.js
+++ modules/system/assets/ui/storm-min.js
@@ -2590,42 +2590,7 @@ var cx=Math.sin(radian)*radius,cy=-Math.cos(radian)*radius;this.hand.setAttribut
 this.input.prop('value',value);if(value!==last){this.input.triggerHandler('change');if(!this.isInput){this.element.trigger('change');}}
 if(this.options.autoclose){this.input.trigger('blur');}
 raiseCallback(this.options.afterDone);};ClockPicker.prototype.remove=function(){this.element.removeData('clockpicker');this.input.off('focus.clockpicker click.clockpicker');this.addon.off('click.clockpicker');if(this.isShown){this.hide();}
-<<<<<<< HEAD
-if(this.isAppended){$win.off('resize.clockpicker'+this.id);this.popover.remove();}};$.fn.clockpicker=function(option){var args=Array.prototype.slice.call(arguments,1);return this.each(function(){var $this=$(this),data=$this.data('clockpicker');if(!data){var options=$.extend({},ClockPicker.DEFAULTS,$this.data(),typeof option=='object'&&option);$this.data('clockpicker',new ClockPicker($this,options));}else{if(typeof data[option]==='function'){data[option].apply(data,args);}}});};}());function applyFocusVisiblePolyfill(scope){var hadKeyboardEvent=true;var hadFocusVisibleRecently=false;var hadFocusVisibleRecentlyTimeout=null;var inputTypesWhitelist={text:true,search:true,url:true,tel:true,email:true,password:true,number:true,date:true,month:true,week:true,time:true,datetime:true,'datetime-local':true};function isValidFocusTarget(el){return!!(el&&el!==document&&el.nodeName!=='HTML'&&el.nodeName!=='BODY'&&'classList'in el&&'contains'in el.classList);}
-function focusTriggersKeyboardModality(el){var type=el.type;var tagName=el.tagName;if(tagName==='INPUT'&&inputTypesWhitelist[type]&&!el.readOnly){return true;}
-if(tagName==='TEXTAREA'&&!el.readOnly){return true;}
-if(el.isContentEditable){return true;}
-return false;}
-function addFocusVisibleClass(el){if(el.classList.contains('focus-visible')){return;}
-el.classList.add('focus-visible');el.setAttribute('data-focus-visible-added','');}
-function removeFocusVisibleClass(el){if(!el.hasAttribute('data-focus-visible-added')){return;}
-el.classList.remove('focus-visible');el.removeAttribute('data-focus-visible-added');}
-function onKeyDown(e){if(e.metaKey||e.altKey||e.ctrlKey){return;}
-if(isValidFocusTarget(scope.activeElement)){addFocusVisibleClass(scope.activeElement);}
-hadKeyboardEvent=true;}
-function onPointerDown(e){hadKeyboardEvent=false;}
-function onFocus(e){if(!isValidFocusTarget(e.target)){return;}
-if(hadKeyboardEvent||focusTriggersKeyboardModality(e.target)){addFocusVisibleClass(e.target);}}
-function onBlur(e){if(!isValidFocusTarget(e.target)){return;}
-if(e.target.classList.contains('focus-visible')||e.target.hasAttribute('data-focus-visible-added')){hadFocusVisibleRecently=true;window.clearTimeout(hadFocusVisibleRecentlyTimeout);hadFocusVisibleRecentlyTimeout=window.setTimeout(function(){hadFocusVisibleRecently=false;window.clearTimeout(hadFocusVisibleRecentlyTimeout);},100);removeFocusVisibleClass(e.target);}}
-function onVisibilityChange(e){if(document.visibilityState==='hidden'){if(hadFocusVisibleRecently){hadKeyboardEvent=true;}
-addInitialPointerMoveListeners();}}
-function addInitialPointerMoveListeners(){document.addEventListener('mousemove',onInitialPointerMove);document.addEventListener('mousedown',onInitialPointerMove);document.addEventListener('mouseup',onInitialPointerMove);document.addEventListener('pointermove',onInitialPointerMove);document.addEventListener('pointerdown',onInitialPointerMove);document.addEventListener('pointerup',onInitialPointerMove);document.addEventListener('touchmove',onInitialPointerMove);document.addEventListener('touchstart',onInitialPointerMove);document.addEventListener('touchend',onInitialPointerMove);}
-function removeInitialPointerMoveListeners(){document.removeEventListener('mousemove',onInitialPointerMove);document.removeEventListener('mousedown',onInitialPointerMove);document.removeEventListener('mouseup',onInitialPointerMove);document.removeEventListener('pointermove',onInitialPointerMove);document.removeEventListener('pointerdown',onInitialPointerMove);document.removeEventListener('pointerup',onInitialPointerMove);document.removeEventListener('touchmove',onInitialPointerMove);document.removeEventListener('touchstart',onInitialPointerMove);document.removeEventListener('touchend',onInitialPointerMove);}
-function onInitialPointerMove(e){if(e.target.nodeName&&e.target.nodeName.toLowerCase()==='html'){return;}
-hadKeyboardEvent=false;removeInitialPointerMoveListeners();}
-document.addEventListener('keydown',onKeyDown,true);document.addEventListener('mousedown',onPointerDown,true);document.addEventListener('pointerdown',onPointerDown,true);document.addEventListener('touchstart',onPointerDown,true);document.addEventListener('visibilitychange',onVisibilityChange,true);addInitialPointerMoveListeners();scope.addEventListener('focus',onFocus,true);scope.addEventListener('blur',onBlur,true);if(scope.nodeType===Node.DOCUMENT_FRAGMENT_NODE&&scope.host){scope.host.setAttribute('data-js-focus-visible','');}else if(scope.nodeType===Node.DOCUMENT_NODE){document.documentElement.classList.add('js-focus-visible');}}
-window.applyFocusVisiblePolyfill=applyFocusVisiblePolyfill;var event;try{event=new CustomEvent('focus-visible-polyfill-ready');}catch(error){event=document.createEvent('CustomEvent');event.initCustomEvent('focus-visible-polyfill-ready',false,false,{});}
-window.dispatchEvent(event);applyFocusVisiblePolyfill(document);(function(){var keyboardeventKeyPolyfill={polyfill:polyfill,keys:{3:'Cancel',6:'Help',8:'Backspace',9:'Tab',12:'Clear',13:'Enter',16:'Shift',17:'Control',18:'Alt',19:'Pause',20:'CapsLock',27:'Escape',28:'Convert',29:'NonConvert',30:'Accept',31:'ModeChange',32:' ',33:'PageUp',34:'PageDown',35:'End',36:'Home',37:'ArrowLeft',38:'ArrowUp',39:'ArrowRight',40:'ArrowDown',41:'Select',42:'Print',43:'Execute',44:'PrintScreen',45:'Insert',46:'Delete',48:['0',')'],49:['1','!'],50:['2','@'],51:['3','#'],52:['4','$'],53:['5','%'],54:['6','^'],55:['7','&'],56:['8','*'],57:['9','('],91:'OS',93:'ContextMenu',106:'*',107:'+',109:'-',110:'.',111:'/',144:'NumLock',145:'ScrollLock',181:'VolumeMute',182:'VolumeDown',183:'VolumeUp',186:[';',':'],187:['=','+'],188:[',','<'],189:['-','_'],190:['.','>'],191:['/','?'],192:['`','~'],219:['[','{'],220:['\\','|'],221:[']','}'],222:["'",'"'],224:'Meta',225:'AltGraph',246:'Attn',247:'CrSel',248:'ExSel',249:'EraseEof',250:'Play',251:'ZoomOut'}};var i;for(i=1;i<25;i++){keyboardeventKeyPolyfill.keys[111+i]='F'+i;}
-var letter='';for(i=65;i<91;i++){letter=String.fromCharCode(i);keyboardeventKeyPolyfill.keys[i]=[letter.toLowerCase(),letter.toUpperCase()];}
-for(i=96;i<106;i++){letter=String.fromCharCode(i-48);keyboardeventKeyPolyfill.keys[i]=letter;}
-function polyfill(){var isEdgeOrIE=(navigator.userAgent.indexOf("MSIE ")>0||!!navigator.userAgent.match(/Trident.*rv\:11\./)||navigator.userAgent.indexOf("Edge/")>0);if(!('KeyboardEvent'in window)||('key'in KeyboardEvent.prototype&&!isEdgeOrIE)){return false;}
-var proto={get:function(x){var key=keyboardeventKeyPolyfill.keys[this.which||this.keyCode];if(Array.isArray(key)){key=key[+this.shiftKey];}
-return key;},enumerable:true,configurable:true};Object.defineProperty(KeyboardEvent.prototype,'key',proto);return proto;}
-if(typeof define==='function'&&define.amd){define('keyboardevent-key-polyfill',keyboardeventKeyPolyfill);}else if(typeof exports!=='undefined'&&typeof module!=='undefined'){module.exports=keyboardeventKeyPolyfill;}else if(window){window.keyboardeventKeyPolyfill=keyboardeventKeyPolyfill;}})();keyboardeventKeyPolyfill.polyfill();+function($){"use strict";if($.oc===undefined)
-=======
 if(this.isAppended){$win.off('resize.clockpicker'+this.id);this.popover.remove();}};$.fn.clockpicker=function(option){var args=Array.prototype.slice.call(arguments,1);return this.each(function(){var $this=$(this),data=$this.data('clockpicker');if(!data){var options=$.extend({},ClockPicker.DEFAULTS,$this.data(),typeof option=='object'&&option);$this.data('clockpicker',new ClockPicker($this,options));}else{if(typeof data[option]==='function'){data[option].apply(data,args);}}});};}());+function($){"use strict";if($.oc===undefined)
->>>>>>> a9072b0d97d96d718667d3b3d6c55375ab2d8dd8
 $.oc={}
 if($.oc.foundation===undefined)
 $.oc.foundation={}
