ba8f80ff471a616caefd5c1bc8a254b941981d98
Samuell <Samuell1@users.noreply.github.com>
2019-09-09 19:57:06 +0200

    Add other tests

---
+++ tests/unit/backend/widgets/FilterTest.php
@@ -0,0 +1,152 @@
+<?php
+
+use Backend\Widgets\Filter;
+use Illuminate\Database\Eloquent\Model;
+
+class FilterTestModel extends Model
+{
+
+}
+
+class FilterTest extends TestCase
+{
+    public function testRestrictedScopeWithUserWithNoPermissions()
+    {
+        $user = factory(Backend\Models\User::class)
+            ->make();
+        $this->actingAs($user);
+
+        $form = $this->restrictedFilterFixture();
+
+        $form->render();
+        $this->assertNull($form->getScope('testRestricted'));
+    }
+
+    public function testRestrictedScopeWithUserWithWrongPermissions()
+    {
+        $user = factory(Backend\Models\User::class)
+            ->make([
+                'permissions' => [
+                    'test.wrong_permission' => 1
+                ]
+            ]);
+        $this->actingAs($user);
+
+        $form = $this->restrictedFilterFixture();
+
+        $form->render();
+        $this->assertNull($form->getScope('testRestricted'));
+    }
+
+    public function testRestrictedScopeWithUserWithRightPermissions()
+    {
+        $user = factory(Backend\Models\User::class)
+            ->make([
+                'permissions' => [
+                    'test.access_field' => 1
+                ]
+            ]);
+        $this->actingAs($user);
+
+        $form = $this->restrictedFilterFixture();
+
+        $form->render();
+        $this->assertNotNull($form->getScope('testRestricted'));
+    }
+
+    public function testRestrictedScopeWithUserWithRightWildcardPermissions()
+    {
+        $user = factory(Backend\Models\User::class)
+            ->make([
+                'permissions' => [
+                    'test.access_field' => 1
+                ]
+            ]);
+        $this->actingAs($user);
+
+        $form = new Filter(null, [
+            'model' => new FormTestModel,
+            'arrayName' => 'array',
+            'scopes' => [
+                'testField' => [
+                    'type' => 'text',
+                    'label' => 'Test 1'
+                ],
+                'testRestricted' => [
+                    'type' => 'text',
+                    'label' => 'Test 2',
+                    'permission' => 'test.*'
+                ]
+            ]
+        ]);
+
+        $form->render();
+        $this->assertNotNull($form->getScope('testRestricted'));
+    }
+
+    public function testRestrictedScopeWithSuperuser()
+    {
+        $user = factory(Backend\Models\User::class)
+            ->states('superuser')
+            ->make();
+        $this->actingAs($user);
+
+        $form = $this->restrictedFilterFixture();
+
+        $form->render();
+        $this->assertNotNull($form->getScope('testRestricted'));
+    }
+
+    public function testRestrictedScopeSinglePermissionWithUserWithWrongPermissions()
+    {
+        $user = factory(Backend\Models\User::class)
+            ->make([
+                'permissions' => [
+                    'test.wrong_permission' => 1
+                ]
+            ]);
+        $this->actingAs($user);
+
+        $form = $this->restrictedFilterFixture(true);
+
+        $form->render();
+        $this->assertNull($form->getScope('testRestricted'));
+    }
+
+    public function testRestrictedScopeSinglePermissionWithUserWithRightPermissions()
+    {
+        $user = factory(Backend\Models\User::class)
+            ->make([
+                'permissions' => [
+                    'test.access_field' => 1
+                ]
+            ]);
+        $this->actingAs($user);
+
+        $form = $this->restrictedFilterFixture(true);
+
+        $form->render();
+        $this->assertNotNull($form->getScope('testRestricted'));
+    }
+
+    protected function restrictedFilterFixture(bool $singlePermission = false)
+    {
+        return new Filter(null, [
+            'model' => new FilterTestModel,
+            'arrayName' => 'array',
+            'scopes' => [
+                'testField' => [
+                    'type' => 'text',
+                    'label' => 'Test 1'
+                ],
+                'testRestricted' => [
+                    'type' => 'text',
+                    'label' => 'Test 2',
+                    'permissions' => ($singlePermission) ? 'test.access_field' : [
+                        'test.access_field'
+                    ]
+                ]
+            ]
+        ]);
+    }
+}
---
+++ tests/unit/backend/widgets/ListsTest.php
@@ -0,0 +1,152 @@
+<?php
+
+use Backend\Widgets\Lists;
+use Illuminate\Database\Eloquent\Model;
+
+class ListsTestModel extends Model
+{
+
+}
+
+class ListsTest extends TestCase
+{
+    public function testRestrictedColumnWithUserWithNoPermissions()
+    {
+        $user = factory(Backend\Models\User::class)
+            ->make();
+        $this->actingAs($user);
+
+        $form = $this->restrictedListsFixture();
+
+        $form->render();
+        $this->assertNull($form->getColumn('testRestricted'));
+    }
+
+    public function testRestrictedColumnWithUserWithWrongPermissions()
+    {
+        $user = factory(Backend\Models\User::class)
+            ->make([
+                'permissions' => [
+                    'test.wrong_permission' => 1
+                ]
+            ]);
+        $this->actingAs($user);
+
+        $form = $this->restrictedListsFixture();
+
+        $form->render();
+        $this->assertNull($form->getColumn('testRestricted'));
+    }
+
+    public function testRestrictedColumnWithUserWithRightPermissions()
+    {
+        $user = factory(Backend\Models\User::class)
+            ->make([
+                'permissions' => [
+                    'test.access_field' => 1
+                ]
+            ]);
+        $this->actingAs($user);
+
+        $form = $this->restrictedListsFixture();
+
+        $form->render();
+        $this->assertNotNull($form->getColumn('testRestricted'));
+    }
+
+    public function testRestrictedColumnWithUserWithRightWildcardPermissions()
+    {
+        $user = factory(Backend\Models\User::class)
+            ->make([
+                'permissions' => [
+                    'test.access_field' => 1
+                ]
+            ]);
+        $this->actingAs($user);
+
+        $form = new Lists(null, [
+            'model' => new FormTestModel,
+            'arrayName' => 'array',
+            'columns' => [
+                'testField' => [
+                    'type' => 'text',
+                    'label' => 'Test 1'
+                ],
+                'testRestricted' => [
+                    'type' => 'text',
+                    'label' => 'Test 2',
+                    'permission' => 'test.*'
+                ]
+            ]
+        ]);
+
+        $form->render();
+        $this->assertNotNull($form->getColumn('testRestricted'));
+    }
+
+    public function testRestrictedColumnWithSuperuser()
+    {
+        $user = factory(Backend\Models\User::class)
+            ->states('superuser')
+            ->make();
+        $this->actingAs($user);
+
+        $form = $this->restrictedListsFixture();
+
+        $form->render();
+        $this->assertNotNull($form->getColumn('testRestricted'));
+    }
+
+    public function testRestrictedColumnSinglePermissionWithUserWithWrongPermissions()
+    {
+        $user = factory(Backend\Models\User::class)
+            ->make([
+                'permissions' => [
+                    'test.wrong_permission' => 1
+                ]
+            ]);
+        $this->actingAs($user);
+
+        $form = $this->restrictedListsFixture(true);
+
+        $form->render();
+        $this->assertNull($form->getColumn('testRestricted'));
+    }
+
+    public function testRestrictedColumnSinglePermissionWithUserWithRightPermissions()
+    {
+        $user = factory(Backend\Models\User::class)
+            ->make([
+                'permissions' => [
+                    'test.access_field' => 1
+                ]
+            ]);
+        $this->actingAs($user);
+
+        $form = $this->restrictedListsFixture(true);
+
+        $form->render();
+        $this->assertNotNull($form->getColumn('testRestricted'));
+    }
+
+    protected function restrictedListsFixture(bool $singlePermission = false)
+    {
+        return new Lists(null, [
+            'model' => new FormTestModel,
+            'arrayName' => 'array',
+            'columns' => [
+                'testField' => [
+                    'type' => 'text',
+                    'label' => 'Test 1'
+                ],
+                'testRestricted' => [
+                    'type' => 'text',
+                    'label' => 'Test 2',
+                    'permissions' => ($singlePermission) ? 'test.access_field' : [
+                        'test.access_field'
+                    ]
+                ]
+            ]
+        ]);
+    }
+}
