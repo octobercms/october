0a71ebf6148dd31b9854b5bc150639b7b6283253
Samuel Georges <sam@daftspunk.com>
2019-09-14 17:29:31 +1000

    Add a small JSON Parser to October framework lib  (#4527)
    * Add a small JSON Parser to October framework lib

--- modules/system/assets/ui/js/autocomplete.js
+++ modules/system/assets/ui/js/autocomplete.js
@@ -378,7 +378,7 @@
         if (typeof value == 'object') return value
 
         try {
-            return JSON.parse(JSON.stringify(eval("({" + value + "})")))
+            return $.oc.JSON("{" + value + "}")
         }
         catch (e) {
             throw new Error('Error parsing the '+name+' attribute value. '+e)
