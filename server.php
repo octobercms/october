638210e8722bfc0162428a50f00cb8148bcf5931
Jenny Ramirez <jendevs@bitbucket.org>
2019-11-10 17:48:10 +0000

    Code quality server.php

--- server.php
+++ server.php
@@ -15,8 +15,7 @@ $uri = urldecode(
  * built-in PHP web server. This provides a convenient way to test a Laravel
  * application without having installed a "real" web server software here.
  */
-if ($uri !== '/' && file_exists(__DIR__.'/'.$uri))
-{
+if ($uri !== '/' && file_exists(__DIR__.'/'.$uri)) {
     return false;
 }
 require_once __DIR__.'/index.php';
