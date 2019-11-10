4efb97730ef61fe2f31194d17501b708e5e75bd7
Jenny Ramirez <jendevs@bitbucket.org>
2019-11-10 17:45:53 +0000

    Tidy up the mess in server.php

--- server.php
+++ server.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
@@ -8,9 +9,12 @@
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
-// This file allows us to emulate Apache's "mod_rewrite" functionality from the
-// built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software here.
+
+/**
+ * This file allows us to emulate Apache's "mod_rewrite" functionality from the
+ * built-in PHP web server. This provides a convenient way to test a Laravel
+ * application without having installed a "real" web server software here.
+ */
 if ($uri !== '/' && file_exists(__DIR__.'/'.$uri))
 {
     return false;
