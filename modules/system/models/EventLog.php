ac28c8a56b59dcf2ce098c4457ecce0ab85916b4
Ayumi Hamasaki <ayumi@github.com>
2020-01-03 00:41:33 +0000

    Update EventLog.php

--- modules/system/models/EventLog.php
+++ modules/system/models/EventLog.php
@@ -84,10 +84,9 @@ class EventLog extends Model
         if (preg_match("/with message '(.+)' in/", $this->message, $match)) {
             return $match[1];
         }
-        elseif (preg_match("/^(.*|\n)/im", trim($this->message), $match)) {
+        elseif (preg_match("/^(.*|\r\n|\r|\n)/im", trim($this->message), $match)) {
             return str_replace('<br>', '', $match[1]);
         }
-
         return Str::limit($this->message, 100);
     }
 }
