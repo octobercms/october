66749f2673996f0446a85d7cfef56d21cef47edf
Ayumi Hamasaki <ayumi@github.com>
2020-01-10 10:46:44 +0000

    revert

--- config/develop.php
+++ config/develop.php
@@ -20,21 +20,5 @@ return [
     */
 
     'decompileBackendAssets' => false,
-    
-    /*
-    |--------------------------------------------------------------------------
-    | Limit event log summary
-    |--------------------------------------------------------------------------
-    |
-    | Enabling this will limit the event log summary to a length of 100
-    | characters.
-    |
-    | true  - limits the summary to 100 characters
-    |
-    | false - displays the whole first sentence summary
-    |
-    */
-
-    'limitEventLogSummary' => false,
 
 ];
