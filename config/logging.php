12f5c1794e2b0a0bac71e3954449857d9ccdc6f2
Ben Thomson <git@alfreido.com>
2020-01-21 08:29:48 +0800

    Use default October log path for system logs

--- config/logging.php
+++ config/logging.php
@@ -42,13 +42,13 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/system.log'),
             'level' => 'debug',
         ],
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/system.log'),
             'level' => 'debug',
             'days' => 14,
         ],
