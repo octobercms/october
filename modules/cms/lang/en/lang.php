14d9415b2fba1791e1c7f6028b860442bddb6e97
Ayumi Hamasaki <ayumi@github.com>
2020-02-03 11:18:50 +0000

    Thanks Luke

--- modules/cms/lang/en/lang.php
+++ modules/cms/lang/en/lang.php
@@ -11,7 +11,7 @@ return [
         'error_deleting' => "Error deleting the template file ':name'. Please check write permissions.",
         'delete_success' => 'Templates deleted: :count.',
         'file_name_required' => 'The File Name field is required.',
-        'safe_mode_enabled' => 'Safe mode is currently enabled (editing the PHP section is disabled) to turn this off, edit the config file: \'cms/enableSafeMode\'.'
+        'safe_mode_enabled' => 'Safe mode is currently enabled. Editing the PHP code of CMS templates is disabled. To disable safe mode, set the `cms.enableSafeMode` configuration value to `false`.',
     ],
     'dashboard' => [
         'active_theme' => [
