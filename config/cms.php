a0ea20fde3eb436d7379cef633ec7120ea0a7a5f
Ayumi Hamasaki <ayumi@github.com>
2020-03-18 14:16:03 +0000

    Update cms.php

--- config/cms.php
+++ config/cms.php
@@ -21,7 +21,7 @@ return [
     | If you are developing with October, it is important to have the latest
     | code base. Set this value to 'true' to tell the platform to download
     | and use the development copies of core files and plugins. This feature
-    | is not intended for use on production mode websites.
+    | is not intended for use on production websites.
     |
     */
 
