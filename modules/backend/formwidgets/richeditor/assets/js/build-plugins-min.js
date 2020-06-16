75b44339cc6d75b7fe47d9d8406b6720ee9913b2
Ben Thomson <git@alfreido.com>
2020-06-16 09:08:42 +0800

    Compile JS

--- modules/backend/formwidgets/richeditor/assets/js/build-plugins-min.js
+++ modules/backend/formwidgets/richeditor/assets/js/build-plugins-min.js
@@ -172,12 +172,11 @@ froalaOptions.tableCellStyles=this.options.tableCellStyles?this.options.tableCel
 froalaOptions.toolbarButtonsMD=froalaOptions.toolbarButtons
 froalaOptions.toolbarButtonsSM=froalaOptions.toolbarButtons
 froalaOptions.toolbarButtonsXS=froalaOptions.toolbarButtons
-if(this.options.allowEmptyTags){let filteredTagList=[];this.options.allowEmptyTags.split(/[\s,]+/).forEach(tagString=>{let i=0;tagString.split('.').forEach(tag=>{if(tag!==null&&!filteredTagList.includes(tag)){filteredTagList.push(i?'.'+tag:tag)}
-i++})})
-froalaOptions.htmlAllowedEmptyTags=filteredTagList}
-if(this.options.allowTags){froalaOptions.htmlAllowedTags=this.options.allowTags.split(/[\s,]+/)}
+if(this.options.allowEmptyTags){froalaOptions.htmlAllowedEmptyTags=[];this.options.allowEmptyTags.split(/[\s,]+/).forEach(function(selector){var tag=selector.split('.',2)
+if(froalaOptions.htmlAllowedEmptyTags.indexOf(tag[0])===-1){froalaOptions.htmlAllowedEmptyTags.push(selector)}})}else{froalaOptions.htmlAllowedEmptyTags=['textarea','a','iframe','object','video','style','script','.fa','.fr-emoticon','.fr-inner','path','line','hr','i']}
+froalaOptions.htmlAllowedTags=this.options.allowTags?this.options.allowTags.split(/[\s,]+/):['a','abbr','address','area','article','aside','audio','b','bdi','bdo','blockquote','br','button','canvas','caption','cite','code','col','colgroup','datalist','dd','del','details','dfn','dialog','div','dl','dt','em','embed','fieldset','figcaption','figure','footer','form','h1','h2','h3','h4','h5','h6','header','hgroup','hr','i','iframe','img','input','ins','kbd','keygen','label','legend','li','link','main','map','mark','menu','menuitem','meter','nav','noscript','object','ol','optgroup','option','output','p','param','pre','progress','queue','rp','rt','ruby','s','samp','script','style','section','select','small','source','span','strike','strong','sub','summary','sup','table','tbody','td','textarea','tfoot','th','thead','time','title','tr','track','u','ul','var','video','wbr']
 froalaOptions.htmlDoNotWrapTags=this.options.noWrapTags?this.options.noWrapTags.split(/[\s,]+/):['figure','script','style']
-if(this.options.removeTags){froalaOptions.htmlRemoveTags=this.options.removeTags.split(/[\s,]+/)}
+froalaOptions.htmlRemoveTags=this.options.removeTags?this.options.removeTags.split(/[\s,]+/):['script','style','base']
 froalaOptions.lineBreakerTags=this.options.lineBreakerTags?this.options.lineBreakerTags.split(/[\s,]+/):['figure, table, hr, iframe, form, dl']
 froalaOptions.shortcutsEnabled=['show','bold','italic','underline','indent','outdent','undo','redo']
 froalaOptions.requestHeaders={'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content'),'X-Requested-With':'XMLHttpRequest'}
@@ -262,13 +261,14 @@ $.fn.richEditor=function(option){var args=Array.prototype.slice.call(arguments,1
 this.each(function(){var $this=$(this)
 var data=$this.data('oc.richEditor')
 var options=$.extend({},RichEditor.DEFAULTS,$this.data(),typeof option=='object'&&option)
-if(!data){$this.data('oc.richEditor',(data=new RichEditor(this,options)))
-if(typeof option=='string'){result=data[option].apply(data,args)
-if(typeof result!='undefined'){return false}}}})
+if(!data)$this.data('oc.richEditor',(data=new RichEditor(this,options)))
+if(typeof option=='string')result=data[option].apply(data,args)
+if(typeof result!='undefined')return false})
 return result?result:this}
 $.fn.richEditor.Constructor=RichEditor
 $.fn.richEditor.noConflict=function(){$.fn.richEditor=old
 return this}
 $(document).render(function(){$('[data-control="richeditor"]').richEditor()})
-if($.oc===undefined){$.oc={}
-$.oc.richEditorButtons=['paragraphFormat','paragraphStyle','quote','bold','italic','align','formatOL','formatUL','insertTable','insertLink','insertImage','insertVideo','insertAudio','insertFile','insertHR','fullscreen','html']}}(window.jQuery);
+if($.oc===undefined)
+$.oc={}
+$.oc.richEditorButtons=['paragraphFormat','paragraphStyle','quote','bold','italic','align','formatOL','formatUL','insertTable','insertLink','insertImage','insertVideo','insertAudio','insertFile','insertHR','fullscreen','html']}(window.jQuery);
