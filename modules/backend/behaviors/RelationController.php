52d9c348c9055ba186d81528cae1411dfc45c580
Alvaro Cánepa <bfpdevel@gmail.com>
2020-07-21 11:48:33 -0300

    Fix updating HasOne relation
    `$this->viewModel` doesn't have `$saveData` attributes, it will be reflected after `$this->viewWidget->setFormValues($saveData);`, but in `$this->viewWidget->model`.
    To save related model with user data, must use `$model` var inside of `$this->viewWidget`.

--- modules/backend/behaviors/RelationController.php
+++ modules/backend/behaviors/RelationController.php
@@ -1157,10 +1157,8 @@ class RelationController extends ControllerBehavior
             }
         }
         elseif ($this->viewMode == 'single') {
-            $this->viewModel = $this->manageWidget->model;
-
             $this->viewWidget->setFormValues($saveData);
-            $this->viewModel->save(null, $this->manageWidget->getSessionKey());
+            $this->viewWidget->model->save(null, $this->manageWidget->getSessionKey());
         }
 
         return $this->relationRefresh();
