2f9a46fa71039fef08886211fcb28c646927e6b4
Ben Thomson <git@alfreido.com>
2020-08-14 11:30:08 +0800

    Fix tests with new algorithm

--- tests/unit/system/classes/FileManifestTest.php
+++ tests/unit/system/classes/FileManifestTest.php
@@ -18,17 +18,17 @@ class FileManifestTest extends TestCase
     public function testGetFiles()
     {
         $this->assertEquals([
-            '/modules/test/file1.php' => '02d1a1b4085289e12e9b025e125ae9f8',
-            '/modules/test/file2.php' => 'a654089f3abf710809c34e2a82ced582',
-            '/modules/test2/file1.php' => 'cd3d6fd5152d3e1cc02d3c2a971f12b3',
+            '/modules/test/file1.php' => '6f9b0b94528a85b2a6bb67b5621e074aef1b4c9fc9ee3ea1bd69100ea14cb3db',
+            '/modules/test/file2.php' => '96ae9f6b6377ad29226ea169f952de49fc29ae895f18a2caed76aeabdf050f1b',
+            '/modules/test2/file1.php' => '94bd47b1ac7b2837b31883ebcd38c8101687321f497c3c4b9744f68ae846721d',
         ], $this->fileManifest->getFiles());
     }
 
     public function testGetModuleChecksums()
     {
         $this->assertEquals([
-            'test' => 'f83bfce9cc5eee731d33ffde215ed8f3',
-            'test2' => 'f6c6421627c68e60fb8e7a47f753b239',
+            'test' => 'c0b794ff210862a4ce16223802efe6e28969f5a4fb42480ec8c2fef2da23d181',
+            'test2' => '32c9f2fb6e0a22dde288a0fe1e4834798360b25e5a91d2597409d9302221381d',
         ], $this->fileManifest->getModuleChecksums());
     }
 
@@ -47,12 +47,12 @@ class FileManifestTest extends TestCase
         $this->fileManifest->setModules(['test']);
 
         $this->assertEquals([
-            '/modules/test/file1.php' => '02d1a1b4085289e12e9b025e125ae9f8',
-            '/modules/test/file2.php' => 'a654089f3abf710809c34e2a82ced582',
+            '/modules/test/file1.php' => '6f9b0b94528a85b2a6bb67b5621e074aef1b4c9fc9ee3ea1bd69100ea14cb3db',
+            '/modules/test/file2.php' => '96ae9f6b6377ad29226ea169f952de49fc29ae895f18a2caed76aeabdf050f1b',
         ], $this->fileManifest->getFiles());
 
         $this->assertEquals([
-            'test' => 'f83bfce9cc5eee731d33ffde215ed8f3',
+            'test' => 'c0b794ff210862a4ce16223802efe6e28969f5a4fb42480ec8c2fef2da23d181',
         ], $this->fileManifest->getModuleChecksums());
     }
 }
--- tests/unit/system/classes/SourceManifestTest.php
+++ tests/unit/system/classes/SourceManifestTest.php
@@ -40,40 +40,40 @@ class SourceManifestTest extends TestCase
             '        {' . "\n" .
             '            "build": 1,' . "\n" .
             '            "modules": {' . "\n" .
-            '                "test": "5f4a1c6efe88be58274591fe067a45c3",' . "\n" .
-            '                "test2": "d41d8cd98f00b204e9800998ecf8427e"' . "\n" .
+            '                "test": "e1d6c6e4c482688e231ee37d89668268426512013695de47bfcb424f9a645c7b",' . "\n" .
+            '                "test2": "a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a"' . "\n" .
             '            },' . "\n" .
             '            "files": {' . "\n" .
             '                "added": {' . "\n" .
-            '                    "\/modules\/test\/file1.php": "02d1a1b4085289e12e9b025e125ae9f8"' . "\n" .
+            '                    "\/modules\/test\/file1.php": "6f9b0b94528a85b2a6bb67b5621e074aef1b4c9fc9ee3ea1bd69100ea14cb3db"' . "\n" .
             '                }' . "\n" .
             '            }' . "\n" .
             '        },' . "\n" .
             '        {' . "\n" .
             '            "build": 2,' . "\n" .
             '            "modules": {' . "\n" .
-            '                "test": "f83bfce9cc5eee731d33ffde215ed8f3",' . "\n" .
-            '                "test2": "f6c6421627c68e60fb8e7a47f753b239"' . "\n" .
+            '                "test": "c0b794ff210862a4ce16223802efe6e28969f5a4fb42480ec8c2fef2da23d181",' . "\n" .
+            '                "test2": "32c9f2fb6e0a22dde288a0fe1e4834798360b25e5a91d2597409d9302221381d"' . "\n" .
             '            },' . "\n" .
             '            "files": {' . "\n" .
             '                "added": {' . "\n" .
-            '                    "\/modules\/test\/file2.php": "a654089f3abf710809c34e2a82ced582",' . "\n" .
-            '                    "\/modules\/test2\/file1.php": "cd3d6fd5152d3e1cc02d3c2a971f12b3"' . "\n" .
+            '                    "\/modules\/test\/file2.php": "96ae9f6b6377ad29226ea169f952de49fc29ae895f18a2caed76aeabdf050f1b",' . "\n" .
+            '                    "\/modules\/test2\/file1.php": "94bd47b1ac7b2837b31883ebcd38c8101687321f497c3c4b9744f68ae846721d"' . "\n" .
             '                }' . "\n" .
             '            }' . "\n" .
             '        },' . "\n" .
             '        {' . "\n" .
             '            "build": 3,' . "\n" .
             '            "modules": {' . "\n" .
-            '                "test": "23dc31cb72204b34662edbb1d446af13",' . "\n" .
-            '                "test2": "f6c6421627c68e60fb8e7a47f753b239"' . "\n" .
+            '                "test": "419a3c073a4296213cdc9319cfc488383753e2e81cefa1c73db38749b82a3c51",' . "\n" .
+            '                "test2": "32c9f2fb6e0a22dde288a0fe1e4834798360b25e5a91d2597409d9302221381d"' . "\n" .
             '            },' . "\n" .
             '            "files": {' . "\n" .
             '                "added": {' . "\n" .
-            '                    "\/modules\/test\/file3.php": "8eaf78ab370eb0e9531bcc92ee29449c"' . "\n" .
+            '                    "\/modules\/test\/file3.php": "7f4132b05911a6b0df4d41bf5dc3d007786b63a5a22daf3060ed222816d57b54"' . "\n" .
             '                },' . "\n" .
             '                "modified": {' . "\n" .
-            '                    "\/modules\/test\/file2.php": "5a753a54244e00662437fad52ee66ffb"' . "\n" .
+            '                    "\/modules\/test\/file2.php": "2c61b2f5688275574251a19a57e06a4eb9e537b3916ebf6f71768e184a4ae538"' . "\n" .
             '                },' . "\n" .
             '                "removed": [' . "\n" .
             '                    "\/modules\/test\/file1.php"' . "\n" .
@@ -108,19 +108,19 @@ class SourceManifestTest extends TestCase
         $this->createManifest();
 
         $this->assertEquals([
-            '/modules/test/file1.php' => '02d1a1b4085289e12e9b025e125ae9f8',
+            '/modules/test/file1.php' => '6f9b0b94528a85b2a6bb67b5621e074aef1b4c9fc9ee3ea1bd69100ea14cb3db',
         ], $this->sourceManifest->getState(1));
 
         $this->assertEquals([
-            '/modules/test/file1.php' => '02d1a1b4085289e12e9b025e125ae9f8',
-            '/modules/test/file2.php' => 'a654089f3abf710809c34e2a82ced582',
-            '/modules/test2/file1.php' => 'cd3d6fd5152d3e1cc02d3c2a971f12b3',
+            '/modules/test/file1.php' => '6f9b0b94528a85b2a6bb67b5621e074aef1b4c9fc9ee3ea1bd69100ea14cb3db',
+            '/modules/test/file2.php' => '96ae9f6b6377ad29226ea169f952de49fc29ae895f18a2caed76aeabdf050f1b',
+            '/modules/test2/file1.php' => '94bd47b1ac7b2837b31883ebcd38c8101687321f497c3c4b9744f68ae846721d',
         ], $this->sourceManifest->getState(2));
 
         $this->assertEquals([
-            '/modules/test/file2.php' => '5a753a54244e00662437fad52ee66ffb',
-            '/modules/test/file3.php' => '8eaf78ab370eb0e9531bcc92ee29449c',
-            '/modules/test2/file1.php' => 'cd3d6fd5152d3e1cc02d3c2a971f12b3',
+            '/modules/test/file2.php' => '2c61b2f5688275574251a19a57e06a4eb9e537b3916ebf6f71768e184a4ae538',
+            '/modules/test/file3.php' => '7f4132b05911a6b0df4d41bf5dc3d007786b63a5a22daf3060ed222816d57b54',
+            '/modules/test2/file1.php' => '94bd47b1ac7b2837b31883ebcd38c8101687321f497c3c4b9744f68ae846721d',
         ], $this->sourceManifest->getState(3));
     }
 
