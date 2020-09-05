eabfd5981a478c58a4cfa8dde9f2418850808f44
panakour <panakourweb@gmail.com>
2020-09-05 12:49:29 +0300

    let app name configured from .env

--- config/app.php
+++ config/app.php
@@ -28,7 +28,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'October CMS',
+    'name' => env('APP_NAME', 'October CMS'),
 
     /*
     |--------------------------------------------------------------------------
