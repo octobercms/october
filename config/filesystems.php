cbb41c5a7659d47f337f7d85899954da221611c8
Teranode <Teranode@users.noreply.github.com>
2021-02-11 10:40:56 -0500

    Change URL for local storage
    When switching to the `Storage` facade the `storage/app` path gets appended twice.

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,7 +46,7 @@ return [
         'local' => [
             'driver' => 'local',
             'root'   => storage_path('app'),
-            'url'    => '/storage/app',
+            'url'    => env('APP_URL'),
         ],
 
         's3' => [
