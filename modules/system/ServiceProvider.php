ab2586d633e551e390cf0fee55d24009cad39168
Saifur Rahman Mohsin <mohsin92@me.com>
2021-02-17 11:53:31 +0530

    Update modules/system/ServiceProvider.php
    Co-authored-by: Luke Towers <github@luketowers.ca>

--- modules/system/ServiceProvider.php
+++ modules/system/ServiceProvider.php
@@ -577,7 +577,7 @@ class ServiceProvider extends ModuleServiceProvider
                 return strtr($message, [':values' => implode(', ', $parameters)]);
             });
 
-            $plugins = PluginManager::instance()->getRegistrationMethodValues('registerValidator');
+            $plugins = PluginManager::instance()->getRegistrationMethodValues('registerValidators');
             foreach ($plugins as $validators) {
                 foreach ($validators as $name => $validator) {
                     if (is_callable($validator)) {
