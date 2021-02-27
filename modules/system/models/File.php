867cd8d2b19bf457c63487c1537b83459d8f1674
Teranode <Teranode@users.noreply.github.com>
2021-02-26 23:18:32 -0500

    Update File.php

--- modules/system/models/File.php
+++ modules/system/models/File.php
@@ -71,13 +71,13 @@ class File extends FileBase
      */
     public function getPublicPath()
     {
-        $uploadsFolder = Config::get('cms.storage.uploads.path', '/storage/app/uploads');
+        $uploadsPath = Config::get('cms.storage.uploads.path', '/storage/app/uploads');
 
         if ($this->isPublic()) {
-            return $this->getDisk()->url($uploadsFolder . '/public/');
+            return $this->getDisk()->url($uploadsPath . '/public/');
         }
         else {
-            return $this->getDisk()->url($uploadsFolder . '/protected/');
+            return $this->getDisk()->url($uploadsPath . '/protected/');
         }
     }
 
