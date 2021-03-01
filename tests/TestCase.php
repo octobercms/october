61df8cf321b9d7f7237b191d8fb51eff57ca9e2a
Ben Thomson <git@alfreido.com>
2021-03-01 23:09:16 +0800

    Add stubs for renamed PHPUnit methods in PHPUnit 9

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,4 +1,7 @@
 <?php
+
+use PHPUnit\Framework\Assert;
+
 class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
     /**
@@ -50,5 +53,39 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
         $property = $class->getProperty($name);
         $property->setAccessible(true);
         return $property->setValue($object, $value);
+    }
+
+    /**
+     * Stub for `assertFileNotExists` to allow compatibility with both PHPUnit 8 and 9.
+     *
+     * @param string $filename
+     * @param string $message
+     * @return void
+     */
+    public static function assertFileNotExists(string $filename, string $message = ''): void
+    {
+        if (method_exists(Assert::class, 'assertFileDoesNotExist')) {
+            Assert::assertFileDoesNotExist($filename, $message);
+            return;
+        }
+
+        Assert::assertFileNotExists($filename, $message);
+    }
+
+    /**
+     * Stub for `assertRegExp` to allow compatibility with both PHPUnit 8 and 9.
+     *
+     * @param string $filename
+     * @param string $message
+     * @return void
+     */
+    public static function assertRegExp(string $pattern, string $string, string $message = ''): void
+    {
+        if (method_exists(Assert::class, 'assertMatchesRegularExpression')) {
+            Assert::assertMatchesRegularExpression($pattern, $string, $message);
+            return;
+        }
+
+        Assert::assertRegExp($pattern, $string, $message);
     }
 }
