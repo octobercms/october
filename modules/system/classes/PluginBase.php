83c63f93b41bd2b69c0943f907acc6b965947cf2
Marten <marten@seequ.nl>
2021-03-18 05:59:30 +0100

    Allow DI in console command constructor (#4920)
    Allow DI in console command constructor

--- modules/system/classes/PluginBase.php
+++ modules/system/classes/PluginBase.php
@@ -279,17 +279,13 @@ class PluginBase extends ServiceProviderBase
      * Registers a new console (artisan) command
      *
      * @param string $key The command name
-     * @param string $class The command class
+     * @param string|\Closure $command The command class or closure
      * @return void
      */
-    public function registerConsoleCommand($key, $class)
+    public function registerConsoleCommand($key, $command)
     {
         $key = 'command.'.$key;
-
-        $this->app->singleton($key, function ($app) use ($class) {
-            return new $class;
-        });
-
+        $this->app->singleton($key, $command);
         $this->commands($key);
     }
 
