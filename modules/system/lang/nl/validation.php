40f54af85f9a605d4a53084b45099108e0ed70a8
Alwin Drenth <adrenth@gmail.com>
2021-03-23 16:45:00 +0100

    Update Dutch (nl) translations

--- modules/system/lang/nl/validation.php
+++ modules/system/lang/nl/validation.php
@@ -9,7 +9,7 @@ return [
     |
     | The following language lines contain the default error messages used by
     | the validator class. Some of these rules have multiple versions such
-    | as the size rules. Feel free to tweak each of these messages.
+    | as the size rules. Feel free to tweak each of these messages here.
     |
     */
 
