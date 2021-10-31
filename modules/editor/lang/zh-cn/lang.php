665de669bc0feac193628c26d6c1eae5075083de
ZhiweiWu0425 <89636889+ZhiweiWu0425@users.noreply.github.com>
2021-10-31 15:00:12 +0800

    Update the Chinese language pack of October 2.0 version (#5625)

---
+++ modules/editor/lang/zh-cn/lang.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => '编辑器'
+    ],
+    'mainview' => [
+        'editor_objects' => '对象',
+        'editor_tabs' => '标签',
+        'editor_close_tab' => '关闭标签',
+    ],
+    'editor' => [
+        'menu_label' => '编辑器',
+    ],
+    'permissions' => [
+        'name' => '编辑',
+        'access_editor' => '打开编辑器工具。 对某些编辑功能的访问，例如编辑 CMS 页面，可以通过其他插件的权限进行管理',
+    ],
+    'common' => [
+        'settings' => '设置',
+        'document_saved' => '文件已保存',
+        'document_deleted' => '文档已删除',
+        'document' => '文档',
+        'document_reloaded' => '文件重新加载',
+        'error_saving' => '保存文档时出错',
+        'error_loading' => '加载文档时出错',
+        'error' => '错误',
+        'discard_changes' => '放弃更改',
+        'confirm_delete' => '文档将被永久删除。',
+        'error_deleting' => '删除文档时出错',
+        'toggle_document_header' => '显示或隐藏文档标题',
+        'reveal_in_sidebar' => '在侧边栏中显示',
+        'apply_and_save' => '应用并保存',
+        'quick_access' => '快速访问',
+        'toggle_sidebar' => '切换侧边栏',
+        'document_not_found' => '找不到请求的文档',
+    ],
+];
--- modules/system/lang/zh-cn/client.php
+++ modules/system/lang/zh-cn/client.php
@@ -33,7 +33,6 @@ return [
         'fullscreen' => '全屏',
         'preview' => '预览',
     ],
-
     'mediamanager' => [
         'insert_link' => "插入链接",
         'insert_image' => "插入图片",
@@ -45,12 +44,14 @@ return [
         'invalid_video_empty_insert' => "请选择要插入的视频文件。",
         'invalid_audio_empty_insert' => "请选择要插入的音频文件。",
     ],
-
     'alert' => [
+        'error' => '错误',
+        'confirm' => '确认',
+        'dismiss' => '取消',
         'confirm_button_text' => '确定',
         'cancel_button_text' => '取消',
+        'widget_remove_confirm' => '删除这个小部件?',
     ],
-
     'datepicker' => [
         'previousMonth' => '上一个月',
         'nextMonth' => '下一个月',
@@ -58,35 +59,54 @@ return [
         'weekdays' => ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
         'weekdaysShort' => ['日', '一', '二', '三', '四', '五', '六']
     ],
-
+    'colorpicker' => [
+        'choose' => '好',
+    ],
     'filter' => [
         'group' => [
-            'all' => '全部'
+            'all' => '全部',
+        ],
+        'scopes' => [
+            'apply_button_text' => '应用',
+            'clear_button_text' => '清除',
         ],
         'dates' => [
             'all' => '全部',
             'filter_button_text' => '筛选',
-            'reset_button_text'  => '重置',
+            'reset_button_text' => '重置',
             'date_placeholder' => '日期',
-            'after_placeholder' => 'After',
-            'before_placeholder' => 'Before'
-        ]
+            'after_placeholder' => '之后',
+            'before_placeholder' => '之前',
+        ],
+        'numbers' => [
+            'all' => '全部',
+            'filter_button_text' => '过滤器',
+            'reset_button_text' => '重置',
+            'min_placeholder' => '最小',
+            'max_placeholder' => '最大',
+        ],
     ],
-
     'eventlog' => [
         'show_stacktrace' => '显示堆栈',
         'hide_stacktrace' => '隐藏堆栈',
         'tabs' => [
-            'formatted' => '格式化的',
+            'formatted' => '格式化',
             'raw' => '原始',
         ],
         'editor' => [
             'title' => '源代码编辑器',
-            'description' => 'Your operating system should be configured to listen to one of these URL schemes.',
-            'openWith' => 'Open with',
-            'remember_choice' => '记住选择',
+            'description' => '您的系统应配置一个侦听这些 URL 的方案',
+            'openWith' => '打开方式',
+            'remember_choice' => '记住本次会话选择的选项',
             'open' => '打开',
-            'cancel' => '取消'
-        ]
-    ]
+            'cancel' => '取消',
+        ],
+    ],
+    'upload' => [
+        'max_files' => '您不能上传任何文件',
+        'invalid_file_type' => '您不能上传这种类型的文件',
+        'file_too_big' => '文件太大 ({{filesize}}MB)。 最大文件大小：{{maxFilesize}}MB',
+        'response_error' => '服务器响应 {{statusCode}} 代码',
+        'remove_file' => '删除文件',
+    ],
 ];
