c101238eba2f6367d8887ef7b12992d78fd298f9
Samuel Georges <sam@daftspunk.com>
2021-11-25 10:16:38 +1100

    Disable core updates by default
    Since v1.1 is installed via composer, this should be the default
    
    Refs #5630

--- config/cms.php
+++ config/cms.php
@@ -131,7 +131,7 @@ return [
     |
     */
 
-    'disableCoreUpdates' => false,
+    'disableCoreUpdates' => true,
 
     /*
     |--------------------------------------------------------------------------
