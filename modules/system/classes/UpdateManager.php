fb94dd5e7d003ccf87497ea11dd095ec7ab0402b
Samuel Georges <sam@daftspunk.com>
2022-02-01 11:52:23 +1100

    Want v3

--- modules/system/classes/UpdateManager.php
+++ modules/system/classes/UpdateManager.php
@@ -14,7 +14,7 @@ class UpdateManager
     /**
      * @var string WANT_VERSION is the default composer version string to use.
      */
-    const WANT_VERSION = 'dev-develop';
+    const WANT_VERSION = '^3.0';
 
     /**
      * Requests details about a project based on its identifier.
