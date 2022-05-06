ac0013103f60c3de43387c1337066db64ffb2ab9
Sam Georges <sam@daftspunk.com>
2022-05-06 10:52:55 +1000

    Sync upstream demo plugin (3.0)

--- plugins/october/demo/Plugin.php
+++ plugins/october/demo/Plugin.php
@@ -1,28 +1,28 @@
 <?php namespace October\Demo;
 
+use System\Classes\PluginBase;
+
 /**
  * The plugin.php file (called the plugin initialization script) defines the plugin information class.
  */
-
-use System\Classes\PluginBase;
-
 class Plugin extends PluginBase
 {
 
     public function pluginDetails()
     {
         return [
-            'name'        => 'October Demo',
+            'name' => 'October Demo',
             'description' => 'Provides features used by the provided demonstration theme.',
-            'author'      => 'Alexey Bobkov, Samuel Georges',
-            'icon'        => 'icon-leaf'
+            'author' => 'Alexey Bobkov, Samuel Georges',
+            'icon' => 'icon-leaf'
         ];
     }
 
     public function registerComponents()
     {
         return [
-            '\October\Demo\Components\Todo' => 'demoTodo'
+            \October\Demo\Components\Todo::class => 'demoTodo',
+            \October\Demo\Components\BackendLink::class => 'backendLink'
         ];
     }
 }
