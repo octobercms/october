48ba977e0586abad48559f07f7898a2b0527a99a
Samuel Georges <sam@daftspunk.com>
2024-02-24 15:08:25 +1100

    Bump to 3.6

--- modules/system/classes/UpdateManager.php
+++ modules/system/classes/UpdateManager.php
@@ -14,7 +14,7 @@ class UpdateManager
     /**
      * @var string WANT_VERSION is the default composer version string to use.
      */
-    const WANT_VERSION = '^3.5';
+    const WANT_VERSION = '^3.6';
 
     /**
      * Requests details about a project based on its identifier.
