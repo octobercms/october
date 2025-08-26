55d1d21ce20accf472213da1d4e897d675750b51
mohammad <mohammad@inktweb.nl>
2025-08-26 16:55:48 +0200

    Trigger action on not empty value when no value passed

--- modules/backend/assets/foundation/scripts/input/input.trigger.js
+++ modules/backend/assets/foundation/scripts/input/input.trigger.js
@@ -118,13 +118,19 @@
                 }
 
                 var self = this;
-                trigger.each(function() {
+                trigger.each(function () {
                     var triggerValue = $(this).val();
 
-                    $.each(Array.isArray(triggerValue) ? triggerValue : [triggerValue], function(key, val) {
-                        triggered = self.matchWildcardConditions(val, self.triggerConditionValue);
-                        return !triggered;
-                    });
+                    // If no specific value is set, treat 'value' as "not empty"
+                    if (!self.triggerConditionValue || self.triggerConditionValue.length === 0 || self.triggerConditionValue[0] === '') {
+                        triggered = triggerValue != '';
+                    } else {
+                        // Otherwise, match against the specific value(s)
+                        $.each(Array.isArray(triggerValue) ? triggerValue : [triggerValue], function (key, val) {
+                            triggered = self.matchWildcardConditions(val, self.triggerConditionValue);
+                            return !triggered; // stop loop if matched
+                        });
+                    }
 
                     return !triggered;
                 });
