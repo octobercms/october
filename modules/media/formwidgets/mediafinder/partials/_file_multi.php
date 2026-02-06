0fea7b5e979855b490fadd6ac3dc4d0e2fc59f8c
Lucas Palomba <ladylain73@gmail.com>
2026-02-06 15:02:08 +0100

    Add new buttons to mediafinder
    For multi mode Mediafinder form widget, adds many buttons :
    - Select / Deselect all
    - Copy selected
    - Paste

--- modules/media/formwidgets/mediafinder/partials/_file_multi.php
+++ modules/media/formwidgets/mediafinder/partials/_file_multi.php
@@ -14,19 +14,45 @@
 
     <div class="mediafinder-control-container <?= $externalToolbarAppState ? 'external-toolbar' : null ?>">
         <div class="mediafinder-control-toolbar">
-            <a href="javascript:;" class="backend-toolbar-button control-button toolbar-find-button">
-                <i class="icon-common-file-star"></i>
-                <span class="button-label"><?= __("Select") ?></span>
-            </a>
+            <div data-control="toolbar">
+                <a href="javascript:;" class="backend-toolbar-button control-button toolbar-find-button">
+                    <i class="icon-common-file-star"></i>
+                    <span class="button-label"><?= __("Select") ?></span>
+                </a>
 
-            <button
-                type="button"
-                class="backend-toolbar-button control-button toolbar-delete-selected populated-only"
-                disabled
-            >
-                <i class="icon-common-file-remove"></i>
-                <span class="button-label"><?= __("Delete Selected") ?> <span></span></span>
-            </button>
+                <button
+                    type="button"
+                    class="backend-toolbar-button control-button toolbar-delete-selected populated-only"
+                    disabled
+                >
+                    <i class="icon-common-file-remove"></i>
+                    <span class="button-label"><?= __("Delete Selected") ?> <span></span></span>
+                </button>
+
+                <button
+                    class="backend-toolbar-button control-button toolbar-select-all populated-only"
+                    disabled
+                >
+                    <i class="icon-check-square"></i>
+                    <span class="button-label"><?= __("Select All") ?> <span></span></span>
+                </button>
+
+                <button
+                    class="backend-toolbar-button control-button toolbar-copy-selected populated-only"
+                    disabled
+                >
+                    <i class="icon-copy"></i>
+                    <span class="button-label"><?= __("Copy Selected") ?> <span></span></span>
+                </button>
+
+                <button
+                    class="backend-toolbar-button control-button toolbar-paste-items"
+                    disabled
+                >
+                    <i class="icon-paste"></i>
+                    <span class="button-label"><?= __("Paste") ?> <span></span></span>
+                </button>
+            </div>
         </div>
 
         <!-- Existing file -->
