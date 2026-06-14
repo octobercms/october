dd70913b7724f637ac8b7ec61b2c924299aa9de5
Scott Bedard <scottbedard90@gmail.com>
2026-06-13 19:55:36 -0500

    cleanup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -28,18 +28,4 @@ php artisan october:migrate --force
 php artisan tailor:migrate
 php artisan theme:seed demo
 
-php artisan tinker --execute="
-if (!System\Models\SiteDefinition::query()->exists()) {
-    \$site = new System\Models\SiteDefinition;
-    \$site->name = 'Default';
-    \$site->code = 'default';
-    \$site->theme = 'demo';
-    \$site->locale = 'en';
-    \$site->is_enabled = true;
-    \$site->is_primary = true;
-    \$site->is_host_restricted = false;
-    \$site->save();
-}
-"
-
 bash .devcontainer/configure-app-url.sh >/dev/null || true
